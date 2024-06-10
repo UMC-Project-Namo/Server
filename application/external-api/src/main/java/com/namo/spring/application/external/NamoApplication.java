19cd6189aab5ad256d47239ee5f4ea5938ca250e
luke0408 <zjvl324@gmail.com>
2024-06-11 01:32:50 +0900

    :recycle: Refactor: 멀티모듈 틀 잡기
    <body>
    - application:exteranl-api : api 비지니스 로직
    - core:namo-common : 프로젝트 전받적으로 사용할 공통 로직
    - clients:social-client : social 로그인 관련 외부 api 호출 로직
    - storage:db-mysql : mysql 관련 도메인 로직
    - storage:db-redis : redis 관련 도메인 로직
    - support:logging : 로그백 관련 로직
    
    <footer>
    - 관련: #134

---
+++ application/external-api/src/main/java/com/namo/spring/application/external/NamoApplication.java
@@ -0,0 +1,13 @@
+package com.namo.spring.application.external;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamoApplication.class, args);
+	}
+
+}
