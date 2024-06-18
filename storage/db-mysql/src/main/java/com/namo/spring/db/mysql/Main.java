4faecda1c8b7f6afc9fe9841f52b371b73d8a912
luke0408 <zjvl324@gmail.com>
2024-06-18 17:40:28 +0900

    :wrench: Chore: Mysql 관련 의존성 및 설정 이동
    <body>
    - build.gradle
      - 의존성 추가
    - JpaConfig
      - Jpa 설정 이동
    
    <footer>
    - 관련: #139

--- storage/db-mysql/src/main/java/com/namo/spring/db/mysql/Main.java
+++
@@ -1,7 +0,0 @@
-package com.namo.spring.db.mysql;
-
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-	}
-}
--- storage/db-mysql/src/main/java/com/namo/spring/db/mysql/config/JpaConfig.java
+++ storage/db-mysql/src/main/java/com/namo/spring/db/mysql/config/JpaConfig.java
@@ -1,9 +1,9 @@
-package com.namo.spring.application.external.global.config.properties;
+package com.namo.spring.db.mysql.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
 @EnableJpaAuditing
-public class JpaAuditingConfig {
+public class JpaConfig {
 }
