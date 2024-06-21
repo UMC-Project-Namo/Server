311124445129a0abbcf4ba3fd76c4569963f7106
luke0408 <zjvl324@gmail.com>
2024-06-22 04:03:27 +0900

    :sparkles: Feat: AuthenticationInterceptor에 JwtUtils 제거 및 JwtAuthHelper 적용
    <footer>
    - 관련: #142

--- application/external-api/src/main/java/com/namo/spring/application/external/global/config/interceptor/AuthenticationInterceptor.java
+++ application/external-api/src/main/java/com/namo/spring/application/external/global/config/interceptor/AuthenticationInterceptor.java
@@ -3,15 +3,17 @@ package com.namo.spring.application.external.global.config.interceptor;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import com.google.gson.Gson;
-import com.namo.spring.application.external.global.utils.JwtUtils;
-import com.namo.spring.core.common.code.status.ErrorStatus;
+import com.namo.spring.application.external.api.user.helper.JwtAuthHelper;
+import com.namo.spring.application.external.global.common.security.jwt.JwtClaimsParserUtil;
+import com.namo.spring.application.external.global.common.security.jwt.access.AccessTokenClaimKeys;
 import com.namo.spring.core.common.exception.AuthException;
+import com.namo.spring.core.infra.common.jwt.JwtClaims;
+import com.namo.spring.core.infra.common.jwt.JwtProvider;
+import com.namo.spring.db.redis.cache.forbidden.ForbiddenTokenService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -21,15 +23,20 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class AuthenticationInterceptor implements HandlerInterceptor {
 
-	private final JwtUtils jwtUtils;
-	private final RedisTemplate<String, String> redisTemplate;
+	private final JwtProvider accessTokenProvider;
+	private final JwtAuthHelper jwtAuthHelper;
+	private final ForbiddenTokenService forbiddenTokenService;
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws
 		Exception {
+
+		String accessToken = jwtAuthHelper.getAccessToken(request);
+		JwtClaims accessTokenClaims = accessTokenProvider.parseJwtClaimsFromToken(accessToken);
+		Long userId = JwtClaimsParserUtil.getClaimValue(accessTokenClaims, AccessTokenClaimKeys.USER_ID.getValue(), Long.class);
+
 		try {
-			Long userId = jwtUtils.resolveRequest(request);
-			validateLogout(jwtUtils.getAccessToken(request));
+			validateLogout(accessToken);
 			request.setAttribute("userId", userId);
 			return true;
 		} catch (AuthException e) {
@@ -43,9 +50,6 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
 	}
 
 	private void validateLogout(String accessToken) {
-		String blackToken = redisTemplate.opsForValue().get(accessToken);
-		if (StringUtils.hasText(blackToken)) {
-			throw new AuthException(ErrorStatus.LOGOUT_ERROR);
-		}
+		forbiddenTokenService.isForbidden(accessToken);
 	}
 }
