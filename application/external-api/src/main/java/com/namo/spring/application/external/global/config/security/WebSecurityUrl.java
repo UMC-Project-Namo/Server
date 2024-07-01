49b236d3fd281bea8aeec0bde4449deb0a5f2e9e
luke0408 <zjvl324@gmail.com>
2024-07-02 05:02:09 +0900

    :sparkles: Feat: url 설정 분리
    <footer>
    - 관련: #153

--- application/external-api/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
+++ application/external-api/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
@@ -1,9 +1,13 @@
 package com.namo.spring.application.external.global.config.security;
 
 public class WebSecurityUrl {
-	protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {};
-	protected static final String[] PUBLIC_ENDPOINTS = {};
+	private WebSecurityUrl() {
+		throw new IllegalStateException("Utility class");
+	}
+
+	protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico"};
+	protected static final String[] PUBLIC_ENDPOINTS = {"/test/**"};
 	protected static final String[] AUTHENTICATED_ENDPOINTS = {};
-	protected static final String[] ANONYMOUS_ENDPOINTS = {};
-	protected static final String[] SWAGGER_ENDPOINTS = {};
+	protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/v1/auths/**"};
+	protected static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger"};
 }
