41880f44cd45a8a1fe0661868efae1827c96d0f6
luke0408 <zjvl324@gmail.com>
2024-07-02 04:48:53 +0900

    :sparkles: Feat: SecurityFilterConfig 설정 구현
    <footer>
    - 관련: #153

--- application/external-api/src/main/java/com/namo/spring/application/external/global/common/security/filter/JwtAuthenticationFilter.java
+++ application/external-api/src/main/java/com/namo/spring/application/external/global/common/security/filter/JwtAuthenticationFilter.java
@@ -12,11 +12,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.namo.spring.application.external.global.common.security.authentication.SecurityUserDetailsService;
 import com.namo.spring.application.external.global.common.security.jwt.access.AccessTokenClaimKeys;
 import com.namo.spring.core.common.code.status.ErrorStatus;
 import com.namo.spring.core.common.exception.JwtException;
@@ -57,7 +57,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-	private final SecurityUserDetailsService securityUserDetailsService;
+	private final UserDetailsService securityUserDetailsService;
 	private final ForbiddenTokenService forbiddenTokenService;
 
 	private final JwtProvider accessTokenProvider;
---
+++ application/external-api/src/main/java/com/namo/spring/application/external/global/config/security/SecurityFilterConfig.java
@@ -0,0 +1,35 @@
+package com.namo.spring.application.external.global.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.namo.spring.application.external.global.common.security.filter.JwtAuthenticationFilter;
+import com.namo.spring.application.external.global.common.security.filter.JwtExceptionFilter;
+import com.namo.spring.core.infra.common.jwt.JwtProvider;
+import com.namo.spring.db.redis.cache.forbidden.ForbiddenTokenService;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+public class SecurityFilterConfig {
+	private final UserDetailsService securityUserDetails;
+	private final ForbiddenTokenService forbiddenTokenService;
+
+	private final JwtProvider accessTokenProvider;
+
+	private final ObjectMapper objectMapper;
+
+	@Bean
+	public JwtExceptionFilter jwtExceptionFilter() {
+		return new JwtExceptionFilter(objectMapper);
+	}
+
+	@Bean
+	public JwtAuthenticationFilter jwtAuthenticationFilter() {
+		return new JwtAuthenticationFilter(securityUserDetails, forbiddenTokenService, accessTokenProvider);
+	}
+}
