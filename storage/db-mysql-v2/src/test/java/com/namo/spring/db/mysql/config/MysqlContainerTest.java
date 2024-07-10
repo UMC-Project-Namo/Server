c03f64d6296257163996931dad680637479140c9
luke0408 <zjvl324@gmail.com>
2024-07-10 19:37:19 +0900

    :sparkles: Feat: MySQL test 환경 구축
    <body>
    - mysql.yml
      - mysql 구현 설정 수정
    - MysqlTestContainerConfig.java
      - Test용 MySQL Container 환경 설정
    - MysqlContainerTest.java
      - MySQL Container 환경 생성 test
    
    <footer>
    - 관련: #175

---
+++ storage/db-mysql-v2/src/test/java/com/namo/spring/db/mysql/config/MysqlContainerTest.java
@@ -0,0 +1,27 @@
+package com.namo.spring.db.mysql.config;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootTest
+public class MysqlContainerTest extends MysqlTestContainerConfig {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	void contextLoads() {
+		assertThat(jdbcTemplate).isNotNull();
+	}
+
+	@Test
+	void testDatabaseConnection() {
+		Integer result = jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+		assertThat(result).isEqualTo(1);
+	}
+}
+
