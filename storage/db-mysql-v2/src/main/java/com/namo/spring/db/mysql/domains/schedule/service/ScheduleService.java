9992bdd7d030f7cf1846cdfdefe8d0ee375626d7
luke0408 <zjvl324@gmail.com>
2024-07-14 17:56:22 +0900

    :sparkles: Feat: Schedule Service 및 Repository 구현
    <footer>
    - 관련: #180

---
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/schedule/service/ScheduleService.java
@@ -0,0 +1,33 @@
+package com.namo.spring.db.mysql.domains.schedule.service;
+
+import java.util.Optional;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.namo.spring.core.common.annotation.DomainService;
+import com.namo.spring.db.mysql.domains.schedule.entity.Schedule;
+import com.namo.spring.db.mysql.domains.schedule.repository.ScheduleRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@DomainService
+@RequiredArgsConstructor
+public class ScheduleService {
+	private final ScheduleRepository scheduleRepository;
+
+	@Transactional
+	public Schedule createSchedule(Schedule schedule) {
+		return scheduleRepository.save(schedule);
+	}
+
+	@Transactional(readOnly = true)
+	public Optional<Schedule> getSchedule(Long id) {
+		return scheduleRepository.findById(id);
+	}
+
+	@Transactional
+	public void deleteSchedule(Long id) {
+		scheduleRepository.deleteById(id);
+	}
+
+}
