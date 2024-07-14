dbb6656ba4da87d5c173eeb2278afc0d2872a8a2
luke0408 <zjvl324@gmail.com>
2024-07-14 17:15:21 +0900

    :sparkles: Feat: Term Service 및 Repository 구현
    <footer>
    - 관련: #180

---
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/service/TermService.java
@@ -0,0 +1,33 @@
+package com.namo.spring.db.mysql.domains.user.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.namo.spring.core.common.annotation.DomainService;
+import com.namo.spring.db.mysql.domains.user.entity.Term;
+import com.namo.spring.db.mysql.domains.user.repository.TermRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@DomainService
+@RequiredArgsConstructor
+public class TermService {
+	private final TermRepository termRepository;
+
+	@Transactional
+	public Term createTerm(Term term) {
+		return termRepository.save(term);
+	}
+
+	@Transactional(readOnly = true)
+	public Optional<List<Term>> readTermListByUserId(Long userId) {
+		return termRepository.findByUserId(userId);
+	}
+
+	@Transactional
+	public void deleteTerm(Long termId) {
+		termRepository.deleteById(termId);
+	}
+}
