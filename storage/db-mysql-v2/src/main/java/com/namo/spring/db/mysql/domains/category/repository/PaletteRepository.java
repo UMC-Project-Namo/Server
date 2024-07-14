b5eecb9877433e33bc7d4f1f3cd8301e1eb6ed34
luke0408 <zjvl324@gmail.com>
2024-07-14 17:30:06 +0900

    :sparkles: Feat: Palette Service 및 Repository 구현
    <footer>
    - 관련: #180

---
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/category/repository/PaletteRepository.java
@@ -0,0 +1,11 @@
+package com.namo.spring.db.mysql.domains.category.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.namo.spring.db.mysql.domains.category.entity.Palette;
+
+public interface PaletteRepository extends JpaRepository<Palette, Long> {
+	List<Palette> findByIdIn(List<Long> paletteIds);
+}
