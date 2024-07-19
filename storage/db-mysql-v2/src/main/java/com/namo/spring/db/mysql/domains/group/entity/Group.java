e86a38bd4d6fbfb7a3819cbc02888a6c40a7b062
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-07-19 21:59:33 +0900

    :sparkles: Feat: GroupUser 매핑 추가
    <footer>
    - 관련: #186

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/group/entity/Group.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/group/entity/Group.java
@@ -1,28 +1,21 @@
 package com.namo.spring.db.mysql.domains.group.entity;
 
-import java.util.Objects;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-
+import com.namo.spring.db.mysql.common.converter.GroupStatusConverter;
+import com.namo.spring.db.mysql.common.model.BaseTimeEntity;
+import com.namo.spring.db.mysql.domains.group.type.GroupStatus;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 import org.springframework.util.StringUtils;
 
-import com.namo.spring.db.mysql.common.converter.GroupStatusConverter;
-import com.namo.spring.db.mysql.common.model.BaseTimeEntity;
-import com.namo.spring.db.mysql.domains.group.type.GroupStatus;
-
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Entity
@@ -31,36 +24,39 @@ import lombok.NoArgsConstructor;
 @DynamicInsert
 public class Group extends BaseTimeEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id", nullable = false)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
 
-	@JdbcTypeCode(SqlTypes.VARCHAR)
-	@Column(name = "name", nullable = false, length = 50)
-	private String name;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
 
-	@JdbcTypeCode(SqlTypes.VARCHAR)
-	@Column(name = "code", nullable = false, length = 50)
-	private String code;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(name = "code", nullable = false, length = 50)
+    private String code;
 
-	@Column(name = "profile_img")
-	private String profileImg;
+    @Column(name = "profile_img")
+    private String profileImg;
 
-	@Convert(converter = GroupStatusConverter.class)
-	@Column(name = "status", nullable = false, length = 50)
-	private GroupStatus status;
+    @Convert(converter = GroupStatusConverter.class)
+    @Column(name = "status", nullable = false, length = 50)
+    private GroupStatus status;
 
-	@Builder
-	public Group(String name, String code, String profileImg, GroupStatus status) {
-		if (!StringUtils.hasText(name))
-			throw new IllegalArgumentException("name은 null이거나 빈 문자열일 수 없습니다.");
-		else if (!StringUtils.hasText(code))
-			throw new IllegalArgumentException("code는 null이거나 빈 문자열일 수 없습니다.");
+    @OneToMany(mappedBy = "group", fetch = FetchType.LAZY)
+    private List<GroupUser> groupUsers = new ArrayList<>();
 
-		this.name = name;
-		this.code = code;
-		this.profileImg = profileImg;
-		this.status = Objects.requireNonNull(status, "status은 null일 수 없습니다.");
-	}
+    @Builder
+    public Group(String name, String code, String profileImg, GroupStatus status) {
+        if (!StringUtils.hasText(name))
+            throw new IllegalArgumentException("name은 null이거나 빈 문자열일 수 없습니다.");
+        else if (!StringUtils.hasText(code))
+            throw new IllegalArgumentException("code는 null이거나 빈 문자열일 수 없습니다.");
+
+        this.name = name;
+        this.code = code;
+        this.profileImg = profileImg;
+        this.status = Objects.requireNonNull(status, "status은 null일 수 없습니다.");
+    }
 }
