9dadbdec9de6f38496e981b09d828df00ef1ab32
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-07-22 02:40:31 +0900

    :sparkles: Feat: dto 수정
    <footer>
    - 관련: #186

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/group/dto/GroupResponse.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/group/dto/GroupResponse.java
@@ -1,51 +1,50 @@
 package com.namo.spring.application.external.api.group.dto;
 
-import java.util.List;
-
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 public class GroupResponse {
-	private GroupResponse() {
-		throw new IllegalStateException("Util Class");
-	}
+    private GroupResponse() {
+        throw new IllegalStateException("Util Class");
+    }
 
-	@AllArgsConstructor
-	@Getter
-	@Builder
-	public static class GroupIdDto {
-		private Long groupId;
-	}
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    public static class GroupIdDto {
+        private Long groupId;
+    }
 
-	@Getter
-	@AllArgsConstructor
-	@Builder
-	public static class GroupDto {
-		private Long groupId;
-		private String groupName;
-		private String groupImgUrl;
-		private String groupCode;
-		private List<GroupUserDto> groupUsers;
-	}
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class GroupDto {
+        private Long groupId;
+        private String groupName;
+        private String groupImgUrl;
+        private String groupCode;
+        private List<GroupUserDto> groupUsers;
+    }
 
-	@Getter
-	@AllArgsConstructor
-	@NoArgsConstructor
-	@Builder
-	public static class GroupUserDto {
-		private Long userId;
-		private String userName;
-		private Integer color;
-	}
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class GroupUserDto {
+        private Long userId;
+        private String userName;
+        private Integer color;
+    }
 
-	@Getter
-	@AllArgsConstructor
-	@NoArgsConstructor
-	@Builder
-	public static class GroupParticipantDto {
-		private Long groupId;
-		private String code;
-	}
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class GroupJoinDto {
+        private Long groupId;
+    }
 }
