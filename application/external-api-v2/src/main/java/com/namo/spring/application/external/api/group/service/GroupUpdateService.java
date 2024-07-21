512ca563893a5fd83e1df3e09bf859512a7def19
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-07-22 02:47:35 +0900

    :sparkles: Feat: GroupUpdateService 구현
    <footer>
    - 관련: #186

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/group/service/GroupUpdateService.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/group/service/GroupUpdateService.java
@@ -1,11 +1,6 @@
 package com.namo.spring.application.external.api.group.service;
 
-import com.namo.spring.core.common.code.status.ErrorStatus;
-import com.namo.spring.core.common.exception.GroupException;
-import com.namo.spring.db.mysql.domains.group.entity.Group;
 import com.namo.spring.db.mysql.domains.group.entity.GroupUser;
-import com.namo.spring.db.mysql.domains.group.exception.GroupUserException;
-import com.namo.spring.db.mysql.domains.group.service.GroupService;
 import com.namo.spring.db.mysql.domains.group.service.GroupUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,15 +11,10 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class GroupUpdateService {
-    private final GroupService groupService;
     private final GroupUserService groupUserService;
 
     @Transactional
-    public Long modifyCustomGroupName(Long userId, Long groupId, String customGroupName) {
-        //TODO: 추후 User 객체 받아오는 코드 추가 필요
-        Group group = groupService.readGroup(groupId).orElseThrow(() -> new GroupException(ErrorStatus.NOT_FOUND_GROUP_FAILURE));
-        GroupUser groupUser = groupUserService.readGroupUserByGroupAndUser(group, user).orElseThrow(() -> new GroupUserException(ErrorStatus.NOT_FOUND_GROUP_USER_FAILURE));
+    public void modifyCustomGroupName(GroupUser groupUser, String customGroupName) {
         groupUser.updateCustomGroupName(customGroupName);
-        return groupId;
     }
 }
