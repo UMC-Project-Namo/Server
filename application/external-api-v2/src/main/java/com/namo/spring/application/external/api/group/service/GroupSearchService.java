b77a27010a193ea25a65b913a9e59eb9e385eb33
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-07-22 10:54:29 +0900

    :fire: Remove: 불필요한 코드 삭제
    <footer>
    - 관련: #186

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/group/service/GroupSearchService.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/group/service/GroupSearchService.java
@@ -1,12 +1,8 @@
 package com.namo.spring.application.external.api.group.service;
 
-import com.namo.spring.core.common.code.status.ErrorStatus;
-import com.namo.spring.db.mysql.domains.group.entity.Group;
 import com.namo.spring.db.mysql.domains.group.entity.GroupUser;
-import com.namo.spring.db.mysql.domains.group.exception.GroupUserException;
 import com.namo.spring.db.mysql.domains.group.service.GroupUserService;
 import com.namo.spring.db.mysql.domains.user.entity.User;
-import com.namo.spring.db.mysql.domains.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,14 +14,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GroupSearchService {
     private final GroupUserService groupUserService;
-    private final UserService userService;
 
     public List<GroupUser> getGroupUsersWithGroupByUser(User user) {
         return groupUserService.readGroupUsersWithGroupByUser(user);
-    }
-
-    public void readGroupUserByGroupAndUser(Group group, User user) {
-        GroupUser groupUser = groupUserService.readGroupUserByGroupAndUser(group, user).orElseThrow(() -> new GroupUserException(ErrorStatus.NOT_FOUND_GROUP_USER_FAILURE));
     }
 
 }
