f112149761d7dd9c1861385e63801a4611c0bafd
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-07-24 18:28:26 +0900

    :fire: Remove: 필요없는 dto 삭제
    <footer>
    - 관련: #209

--- storage/db-mysql/src/main/java/com/namo/spring/db/mysql/domains/individual/dto/ScheduleProjection.java
+++ storage/db-mysql/src/main/java/com/namo/spring/db/mysql/domains/individual/dto/ScheduleProjection.java
@@ -1,13 +1,10 @@
 package com.namo.spring.db.mysql.domains.individual.dto;
 
-import com.namo.spring.db.mysql.domains.individual.domain.Image;
 import com.namo.spring.db.mysql.domains.individual.domain.Schedule;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 public class ScheduleProjection {
     @Getter
@@ -26,14 +23,5 @@ public class ScheduleProjection {
         private Schedule schedule;
         private Long categoryId;
         private Long color;
-    }
-
-    @AllArgsConstructor
-    @Getter
-    @Builder
-    public static class DiaryByUserDto {
-        private Long scheduleId;
-        private String contents;
-        private List<Image> images;
     }
 }
