104b77bb51a92a770a2eab3fceb891421765f516
hosung-222 <gik11kr@naver.com>
2024-08-13 00:20:57 +0900

    :sparkles: Feat: 회원 관리 서비스용 dto 구현
    <body>
    - Record 타입의 MemberCreationRecord 구현
    
    - 관련 : #246

---
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/dto/MemberDto.java
@@ -0,0 +1,9 @@
+package com.namo.spring.application.external.api.user.dto;
+
+import com.namo.spring.db.mysql.domains.user.entity.Member;
+
+public class MemberDto {
+	public record MemberCreationRecord(Member member, boolean isNewUser) {
+	}
+
+}
