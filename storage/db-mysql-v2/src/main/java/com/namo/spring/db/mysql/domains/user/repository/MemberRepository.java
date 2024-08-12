44572a75866acf45ce0068837fd3a61ae9d69659
hosung-222 <gik11kr@naver.com>
2024-08-12 17:05:40 +0900

    :sparkles: Feat: Member, Term 관련 jpa 메서드 구현
    <body>
    - Member, Term DB 조회 로직 구현
    
    - 관련 : #246

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/repository/MemberRepository.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/repository/MemberRepository.java
@@ -1,8 +1,27 @@
 package com.namo.spring.db.mysql.domains.user.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.namo.spring.db.mysql.domains.user.entity.Member;
+import com.namo.spring.db.mysql.domains.user.type.MemberStatus;
+import com.namo.spring.db.mysql.domains.user.type.SocialType;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+	Optional<Member> findMemberByEmail(String email);
+
+	Optional<Member> findMemberByEmailAndSocialType(String email, SocialType socialType);
+
+	Optional<Member> findMemberBySocialRefreshToken(String refreshToken);
+
+	@Query("select u from Member u where u.id in :ids")
+	List<Member> findMembersById(List<Long> ids);
+
+	@Query("select u from Member u where u.status in :status and u.updatedAt < :localDateTime")
+	List<Member> findMembersByStatusAndDate(MemberStatus status, LocalDateTime localDateTime);
 }
