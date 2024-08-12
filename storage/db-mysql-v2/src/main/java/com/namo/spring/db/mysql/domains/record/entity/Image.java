6db6e4c3ac5cabf7b20309388a0f429b4577b6aa
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-12 11:43:04 +0900

    :sparkles: Feat: 리턴 타입 수정
    <footer>
    - 관련: #230

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/record/entity/Image.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/record/entity/Image.java
@@ -1,9 +1,9 @@
 package com.namo.spring.db.mysql.domains.record.entity;
 
 public interface Image {
-	void updateImageOrder(Integer imageOrder);
+    void updateImageOrder(Integer imageOrder);
 
-	void updateImageUrl(String imageUrl);
+    void updateImageUrl(String imageUrl);
 
-	void getImageUrl();
+    String getImageUrl();
 }
