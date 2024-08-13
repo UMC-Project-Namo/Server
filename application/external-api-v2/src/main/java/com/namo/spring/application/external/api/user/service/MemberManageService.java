c30a0a9ebe3e7e1e2bfd7b7963fd74e0b0be3c10
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-13 23:51:10 +0900

    :art: Style: individual -> personal 변경
    <footer>
    - 관련: #250

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/service/MemberManageService.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/service/MemberManageService.java
@@ -1,11 +1,5 @@
 package com.namo.spring.application.external.api.user.service;
 
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.stereotype.Service;
-
 import com.namo.spring.application.external.api.Category.service.CategoryMaker;
 import com.namo.spring.application.external.api.user.converter.MemberConverter;
 import com.namo.spring.application.external.api.user.dto.MemberDto;
@@ -19,81 +13,85 @@ import com.namo.spring.db.mysql.domains.user.repository.TermRepository;
 import com.namo.spring.db.mysql.domains.user.service.MemberService;
 import com.namo.spring.db.mysql.domains.user.type.MemberStatus;
 import com.namo.spring.db.mysql.domains.user.type.SocialType;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberManageService {
 
-	private final MemberRepository memberRepository;
-	private final TermRepository termRepository;
-	private final MemberService memberService;
-	private final CategoryMaker categoryMaker;
+    private final MemberRepository memberRepository;
+    private final TermRepository termRepository;
+    private final MemberService memberService;
+    private final CategoryMaker categoryMaker;
 
-	public List<Term> getTerms(Member member) {
-		return termRepository.findTermsByMember(member);
-	}
+    public List<Term> getTerms(Member member) {
+        return termRepository.findTermsByMember(member);
+    }
 
-	public Member getMember(Long memberId) {
-		return memberRepository.findById(memberId)
-			.orElseThrow(() -> new MemberException(ErrorStatus.NOT_FOUND_USER_FAILURE));
-	}
+    public Member getMember(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new MemberException(ErrorStatus.NOT_FOUND_USER_FAILURE));
+    }
 
-	public Optional<Member> getMemberByEmailAndSocialType(String email, SocialType socialType) {
-		return memberRepository.findMemberByEmailAndSocialType(email, socialType);
-	}
+    public Optional<Member> getMemberByEmailAndSocialType(String email, SocialType socialType) {
+        return memberRepository.findMemberByEmailAndSocialType(email, socialType);
+    }
 
-	public List<Member> getInactiveMember() {
-		return memberRepository.findMembersByStatusAndDate(MemberStatus.INACTIVE, LocalDateTime.now().minusDays(3));
-	}
+    public List<Member> getInactiveMember() {
+        return memberRepository.findMembersByStatusAndDate(MemberStatus.INACTIVE, LocalDateTime.now().minusDays(3));
+    }
 
-	public void removeMember(Member member) {
-		memberRepository.delete(member);
-	}
+    public void removeMember(Member member) {
+        memberRepository.delete(member);
+    }
 
-	public void checkEmailAndName(String email, String name) {
-		if (email.isBlank() || name.isBlank()) {
-			throw new MemberException(ErrorStatus.USER_POST_ERROR);
-		}
-	}
+    public void checkEmailAndName(String email, String name) {
+        if (email.isBlank() || name.isBlank()) {
+            throw new MemberException(ErrorStatus.USER_POST_ERROR);
+        }
+    }
 
-	public MemberDto.MemberCreationRecord updateExistingMember(Member existingMember, String socialRefreshToken) {
-		existingMember.changeToActive();
-		existingMember.updateSocialRefreshToken(socialRefreshToken);
-		return new MemberDto.MemberCreationRecord(existingMember, false);
-	}
+    public MemberDto.MemberCreationRecord updateExistingMember(Member existingMember, String socialRefreshToken) {
+        existingMember.changeToActive();
+        existingMember.updateSocialRefreshToken(socialRefreshToken);
+        return new MemberDto.MemberCreationRecord(existingMember, false);
+    }
 
-	public MemberDto.MemberCreationRecord createNewMember(Member member) {
-		checkEmailAndName(member.getEmail(), member.getName());
-		log.debug("Creating new social member");
-		Member savedMember = memberService.createMember(member);
-		makeBaseCategory(savedMember);
-		return new MemberDto.MemberCreationRecord(savedMember, true);
-	}
+    public MemberDto.MemberCreationRecord createNewMember(Member member) {
+        checkEmailAndName(member.getEmail(), member.getName());
+        log.debug("Creating new social member");
+        Member savedMember = memberService.createMember(member);
+        makeBaseCategory(savedMember);
+        return new MemberDto.MemberCreationRecord(savedMember, true);
+    }
 
-	public Member createNewAppleMember(MemberRequest.AppleSignUpDto req, String email, String appleRefreshToken) {
-		checkEmailAndName(email, req.getUsername());
-		log.debug("Creating new apple member");
-		Member newMember = memberService.createMember(MemberConverter.toMember(
-			email,
-			req.getUsername(),
-			appleRefreshToken,
-			SocialType.APPLE));
-		makeBaseCategory(newMember);
-		return newMember;
-	}
+    public Member createNewAppleMember(MemberRequest.AppleSignUpDto req, String email, String appleRefreshToken) {
+        checkEmailAndName(email, req.getUsername());
+        log.debug("Creating new apple member");
+        Member newMember = memberService.createMember(MemberConverter.toMember(
+                email,
+                req.getUsername(),
+                appleRefreshToken,
+                SocialType.APPLE));
+        makeBaseCategory(newMember);
+        return newMember;
+    }
 
-	public Member updateExistingAppleMember(Member existingMember, String appleRefreshToken) {
-		existingMember.changeToActive();
-		existingMember.updateSocialRefreshToken(appleRefreshToken);
-		return existingMember;
-	}
+    public Member updateExistingAppleMember(Member existingMember, String appleRefreshToken) {
+        existingMember.changeToActive();
+        existingMember.updateSocialRefreshToken(appleRefreshToken);
+        return existingMember;
+    }
 
-	private void makeBaseCategory(Member member) {
-		categoryMaker.makeIndividualCategory(member);
-		categoryMaker.makeGroupCategory(member);
-	}
+    private void makeBaseCategory(Member member) {
+        categoryMaker.makePersonalCategory(member);
+        categoryMaker.makeGroupCategory(member);
+    }
 }
