7db353a658ae6c6d7906ce3880bd6e8d65b41e30
hosung-222 <gik11kr@naver.com>
2024-08-13 15:54:48 +0900

    :sparkles: Feat: key decoder 구현
    <body>
    - decode (string -> BigInteger)
    
    - 관련 : #246

---
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/service/KeyDecoder.java
@@ -0,0 +1,14 @@
+package com.namo.spring.application.external.api.user.service;
+
+import java.math.BigInteger;
+import java.util.Base64;
+
+public class KeyDecoder {
+
+	private static final int POSITIVE_SIG_NUM = 1;
+
+	public static BigInteger decodeToBigInteger(String base64String) {
+		byte[] decoded = Base64.getUrlDecoder().decode(base64String);
+		return new BigInteger(POSITIVE_SIG_NUM, decoded);
+	}
+}
