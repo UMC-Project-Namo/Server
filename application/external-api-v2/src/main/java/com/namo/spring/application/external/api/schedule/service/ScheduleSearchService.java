58313fecfabd17b980b86a47aa726e0afab6fb94
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-13 20:45:07 +0900

    :sparkles: Feat: ScheduleSearchService 구현
    <footer>
    - 관련: #247

---
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/service/ScheduleSearchService.java
@@ -0,0 +1,21 @@
+package com.namo.spring.application.external.api.schedule.service;
+
+import com.namo.spring.db.mysql.domains.schedule.dto.MeetingScheduleQueryDto;
+import com.namo.spring.db.mysql.domains.schedule.service.ScheduleService;
+import com.namo.spring.db.mysql.domains.user.entity.Member;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ScheduleSearchService {
+    private ScheduleService scheduleService;
+
+    public List<MeetingScheduleQueryDto> getMeetingSchedulesByMember(Member member) {
+        return scheduleService.readMeetingSchedulesWithParticipantsByMember(member);
+    }
+}
