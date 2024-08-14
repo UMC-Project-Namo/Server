1219f4a8c723cc2169f0684d2e98cb1cc0a2c14c
JOO WON SEO <joowojr@gmail.com>
2024-08-14 20:11:49 +0900

    :bug: Fix: Swagger 경로 재설정

--- application/external-api/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
+++ application/external-api/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
@@ -13,8 +13,8 @@ public class WebSecurityUrl {
     };
     protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/v1/auths/**"};
     protected static final String[] SWAGGER_ENDPOINTS = {
-            "/api-docs/**", "/v3/api-docs/**",
-            "/swagger-ui/**", "/swagger"
+            "/api/v1/api-docs/**", "/api/v1/v3/api-docs/**",
+            "/api/v1/swagger-ui/**", "/api/v1/swagger"
     };
     protected static final String[] REISSUANCE_ENDPOINTS = {"/api/v1/auths/reissuance"};
 }
