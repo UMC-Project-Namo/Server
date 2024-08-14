4fb9ee04df98018cf685ddc6293dc748f2ec4948
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-14 18:39:25 +0900

    :construction_worker: Chore: v2 API swagger 설정
    <footer>
    - 관련: #253

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
@@ -1,15 +1,14 @@
 package com.namo.spring.application.external.global.config.security;
 
 public class WebSecurityUrl {
-	private WebSecurityUrl() {
-		throw new IllegalStateException("Utility class");
-	}
+    private WebSecurityUrl() {
+        throw new IllegalStateException("Utility class");
+    }
 
-	protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico"};
-	protected static final String[] PUBLIC_ENDPOINTS = {"/test/**"};
-	protected static final String[] AUTHENTICATED_ENDPOINTS = {};
-	protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/v2/auths/**"};
-	protected static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**",
-		"/swagger"};
-	protected static final String[] REISSUANCE_ENDPOINTS = {"/api/v1/auths/reissuance"};
+    protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico"};
+    protected static final String[] PUBLIC_ENDPOINTS = {"/test/**"};
+    protected static final String[] AUTHENTICATED_ENDPOINTS = {};
+    protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/v2/auths/**"};
+    protected static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/api/v2/swagger-ui/**", "/api/v2/swagger"};
+    protected static final String[] REISSUANCE_ENDPOINTS = {"/api/v1/auths/reissuance"};
 }
