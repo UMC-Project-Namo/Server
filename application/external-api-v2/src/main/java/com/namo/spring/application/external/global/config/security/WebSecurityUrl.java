1af87f89c805346baa0a8b0c83b35187da5e7788
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-14 19:29:40 +0900

    :bug: Fix: Swagger 경로 추가
    <footer>
    - 관련: #253

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
@@ -9,6 +9,6 @@ public class WebSecurityUrl {
     protected static final String[] PUBLIC_ENDPOINTS = {"/test/**"};
     protected static final String[] AUTHENTICATED_ENDPOINTS = {};
     protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/v2/auths/**"};
-    protected static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/api/v2/swagger-ui/**", "/api/v2/swagger"};
+    protected static final String[] SWAGGER_ENDPOINTS = {"/api-docs/**", "/v3/api-docs/**", "/swagger-ui/**", "/api/v2/swagger-ui/**", "/api/v2/swagger"};
     protected static final String[] REISSUANCE_ENDPOINTS = {"/api/v1/auths/reissuance"};
 }
