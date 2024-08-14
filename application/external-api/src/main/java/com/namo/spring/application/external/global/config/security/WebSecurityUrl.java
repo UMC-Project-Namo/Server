403666533f7692482abe0d99d5c3edc631cd9c15
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-14 18:39:09 +0900

    :construction_worker: Chore: v1 API swagger 설정
    <footer>
    - 관련: #253

--- application/external-api/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
+++ application/external-api/src/main/java/com/namo/spring/application/external/global/config/security/WebSecurityUrl.java
@@ -1,20 +1,20 @@
 package com.namo.spring.application.external.global.config.security;
 
 public class WebSecurityUrl {
-	private WebSecurityUrl() {
-		throw new IllegalStateException("Utility class");
-	}
+    private WebSecurityUrl() {
+        throw new IllegalStateException("Utility class");
+    }
 
-	protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico"};
-	protected static final String[] PUBLIC_ENDPOINTS = {"/test/**", "/error"};
-	protected static final String[] AUTHENTICATED_ENDPOINTS = {
-		"/api/v1/auths/logout",
-		"/api/v1/auths/kakao/delete", "/api/v1/auths/naver/delete", "/api/v1/auths/apple/delete"
-	};
-	protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/v1/auths/**"};
-	protected static final String[] SWAGGER_ENDPOINTS = {
-		"/api-docs/**", "/v3/api-docs/**",
-		"/swagger-ui/**", "/swagger"
-	};
-	protected static final String[] REISSUANCE_ENDPOINTS = {"/api/v1/auths/reissuance"};
+    protected static final String[] READ_ONLY_PUBLIC_ENDPOINTS = {"/favicon.ico"};
+    protected static final String[] PUBLIC_ENDPOINTS = {"/test/**", "/error"};
+    protected static final String[] AUTHENTICATED_ENDPOINTS = {
+            "/api/v1/auths/logout",
+            "/api/v1/auths/kakao/delete", "/api/v1/auths/naver/delete", "/api/v1/auths/apple/delete"
+    };
+    protected static final String[] ANONYMOUS_ENDPOINTS = {"/api/v1/auths/**"};
+    protected static final String[] SWAGGER_ENDPOINTS = {
+            "/api-docs/**", "/v3/api-docs/**",
+            "/api/v1/swagger-ui/**", "/api/v1/swagger"
+    };
+    protected static final String[] REISSUANCE_ENDPOINTS = {"/api/v1/auths/reissuance"};
 }
