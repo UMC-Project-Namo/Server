658b012797ee09984b07445c659589e87f260d5e
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-15 15:09:52 +0900

    :sparkles: Feat: PersonalScheduleResponseConverter 구현
    <footer>
    - 관련: #250

---
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/converter/PersonalScheduleResponseConverter.java
@@ -0,0 +1,8 @@
+package com.namo.spring.application.external.api.schedule.converter;
+
+public class PersonalScheduleResponseConverter {
+    private PersonalScheduleResponseConverter() {
+        throw new IllegalStateException("Util Class");
+    }
+
+}
