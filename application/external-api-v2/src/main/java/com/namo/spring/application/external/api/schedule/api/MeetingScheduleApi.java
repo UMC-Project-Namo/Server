db2f31d82b37c1f7b6cb9aef636487c930fc9f06
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-15 23:36:33 +0900

    :memo: Docs: MeetingSchedule API docs 구현
    <footer>
    - 관련: #250

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/api/MeetingScheduleApi.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/api/MeetingScheduleApi.java
@@ -18,7 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "모임", description = "모임 일정 관련 API")
 public interface MeetingScheduleApi {
-    @Operation(summary = "모임 일정 생성", description = "그룹의 모임 일정을 생성합니다. 요청 성공 시 모임 일정 ID를 전송합니다.")
+    @Operation(summary = "모임 일정 생성", description = "모임 일정을 생성합니다. 요청 성공 시 모임 일정 ID를 전송합니다.")
     @ApiErrorCodes(value = {ErrorStatus.EMPTY_ACCESS_KEY, ErrorStatus.EXPIRATION_ACCESS_TOKEN, ErrorStatus.EXPIRATION_REFRESH_TOKEN, ErrorStatus.INTERNET_SERVER_ERROR})
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", examples = {@ExampleObject(name = "모임 일정 생성 성공", value = """
             {
@@ -32,15 +32,29 @@ public interface MeetingScheduleApi {
             {
             	"isSuccess": false,
             	"code": 404,
-            	"message": "모임 일정에 참여할 유저가 없습니다."
+            	"message": "유저를 찾을 수 없습니다."
             }
             """), @ExampleObject(name = "요청 실패 - 모임 일정 카테고리를 찾을 수 없습니다. ", value = """
             {
             	"isSuccess": false,
             	"code": 404,
-            	"message": "유저가 그룹에 포함되어 있지 않습니다."
+            	"message": "카테고리를 찾을 수 없습니다."
             }
-            """)}))
+            """),
+            @ExampleObject(name = "요청 실패 - 시작 날짜가 종료 날짜 이전 이어야 합니다. ", value = """
+                    {
+                    	"isSuccess": false,
+                    	"code": 404,
+                    	"message": "시작 날짜가 종료 날짜 이전 이어야 합니다."
+                    }
+                    """),
+            @ExampleObject(name = "요청 실패 - 모임 일정에 대한 이미지 파일 업로인 과정에서 오류가 발생하였습니다. ", value = """
+                    {
+                    	"isSuccess": false,
+                    	"code": 404,
+                    	"message": "파일 업로드 과정에서 오류가 발생하였습니다."
+                    }
+                    """)}))
     ResponseDto<Long> createMeetingSchedule(@Valid @RequestPart ScheduleRequest.PostMeetingScheduleDto dto,
                                             @RequestPart(required = false) MultipartFile image,
                                             @AuthenticationPrincipal SecurityUserDetails member);
