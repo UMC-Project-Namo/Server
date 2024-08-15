04759cc87c510e6d0c88e8e13ac62ce6cc25b5c3
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-15 13:24:54 +0900

    :art: Style: user-> member 변경
    <footer>
    - 관련: #250

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/service/MemberService.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/service/MemberService.java
@@ -1,37 +1,37 @@
 package com.namo.spring.db.mysql.domains.user.service;
 
-import java.util.Optional;
-
-import org.springframework.transaction.annotation.Transactional;
-
 import com.namo.spring.core.common.annotation.DomainService;
+import com.namo.spring.core.common.code.status.ErrorStatus;
 import com.namo.spring.db.mysql.domains.user.entity.Member;
+import com.namo.spring.db.mysql.domains.user.exception.MemberException;
 import com.namo.spring.db.mysql.domains.user.repository.MemberRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @DomainService
 @RequiredArgsConstructor
 public class MemberService {
-	private final MemberRepository userRepository;
+    private final MemberRepository memberRepository;
 
-	@Transactional
-	public Member createMember(Member member) {
-		return userRepository.save(member);
-	}
+    @Transactional
+    public Member createMember(Member member) {
+        return memberRepository.save(member);
+    }
 
-	@Transactional(readOnly = true)
-	public Optional<Member> readMember(Long memberId) {
-		return userRepository.findById(memberId);
-	}
+    @Transactional(readOnly = true)
+    public Member readMember(Long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(
+                () -> new MemberException(ErrorStatus.NOT_FOUND_USER_FAILURE)
+        );
+    }
 
-	@Transactional
-	public void deleteMember(Long memberId) {
-		userRepository.deleteById(memberId);
-	}
+    @Transactional
+    public void deleteMember(Long memberId) {
+        memberRepository.deleteById(memberId);
+    }
 
-	@Transactional
-	public void deleteMember(Member member) {
-		userRepository.delete(member);
-	}
+    @Transactional
+    public void deleteMember(Member member) {
+        memberRepository.delete(member);
+    }
 }
