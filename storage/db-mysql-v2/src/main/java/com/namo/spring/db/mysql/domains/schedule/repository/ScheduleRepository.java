91b7548b9f35399de99a734042f0aad6b55d193e
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-15 13:22:42 +0900

    :sparkles: Feat: 참여 상태 조건 추가
    <footer>
    - 관련: #250

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/schedule/repository/ScheduleRepository.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/schedule/repository/ScheduleRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
 
-    @Query("SELECT NEW com.namo.spring.db.mysql.domains.schedule.dto.MeetingScheduleQueryDto(s.id, s.title, p, COUNT(p)) FROM Schedule s JOIN s.participantList p WHERE p.member = :member AND s.scheduleType = 1 ORDER BY s.period.startDate")
+    @Query("SELECT NEW com.namo.spring.db.mysql.domains.schedule.dto.MeetingScheduleQueryDto(s.id, s.title, p, COUNT(p)) FROM Schedule s JOIN s.participantList p WHERE p.member = :member AND s.scheduleType = 1 AND p.status = 'ACTIVE' ORDER BY s.period.startDate")
     List<MeetingScheduleQueryDto> findMeetingSchedulesWithParticipantsByMember(Member member);
 }
