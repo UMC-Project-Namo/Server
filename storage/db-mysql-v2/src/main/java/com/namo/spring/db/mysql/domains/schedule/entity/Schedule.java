ca42551414e86f1048fe9e0de309248f4359083f
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-17 22:52:24 +0900

    :sparkles: Feat : 컬럼 추가
    <body>
    - participantNicknames
    - participantCount
    - invitationUrl
    
    <footer>
    - 관련: #268

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/schedule/entity/Schedule.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/schedule/entity/Schedule.java
@@ -14,7 +14,9 @@ import org.hibernate.type.SqlTypes;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Entity
@@ -44,11 +46,20 @@ public class Schedule extends BaseTimeEntity {
     @JdbcTypeCode(SqlTypes.VARCHAR)
     private String imageUrl;
 
+    @Column(nullable = true)
+    private Integer participantCount;
+
+    @Column(nullable = true)
+    private String participantNicknames;
+
+    @Column(nullable = true)
+    private String invitationUrl;
+
     @OneToMany(mappedBy = "schedule", fetch = FetchType.LAZY)
     private List<Participant> participantList = new ArrayList<>();
 
     @Builder
-    public Schedule(String title, Period period, Location location, int scheduleType, String imageUrl) {
+    public Schedule(String title, Period period, Location location, int scheduleType, String imageUrl, Integer participantCount, String participantNicknames) {
         if (!StringUtils.hasText(title))
             throw new IllegalArgumentException("title은 null이거나 빈 문자열일 수 없습니다.");
         this.title = title;
@@ -56,15 +67,61 @@ public class Schedule extends BaseTimeEntity {
         this.location = location;
         this.scheduleType = scheduleType;
         this.imageUrl = imageUrl;
+        this.participantCount = participantCount;
+        this.participantNicknames = participantNicknames;
     }
 
-    public static Schedule of(String title, Period period, Location location, int scheduleType, String imageUrl) {
+    public static Schedule of(String title, Period period, Location location, int scheduleType, String imageUrl, Integer participantCount, String participantNicknames) {
         return Schedule.builder()
                 .title(title)
                 .period(period)
                 .location(location)
                 .scheduleType(scheduleType)
                 .imageUrl(imageUrl)
+                .participantCount(participantCount)
+                .participantNicknames(participantNicknames)
                 .build();
     }
+
+    public void addActiveParticipant(String nickname) {
+        if (!StringUtils.hasText(nickname))
+            throw new IllegalArgumentException("nickname은 null이거나 빈 문자열일 수 없습니다.");
+        if (this.participantNicknames == null || this.participantNicknames == "") {
+            this.participantNicknames = nickname;
+        } else {
+            this.participantNicknames += "," + nickname;
+        }
+        if (this.participantCount == null) {
+            this.participantCount = 1;
+        } else {
+            this.participantCount++;
+        }
+    }
+
+    public void updateParticipant(String oldNickname, String newNickname) {
+        if (!StringUtils.hasText(oldNickname) || !StringUtils.hasText(newNickname))
+            throw new IllegalArgumentException("nickname은 null이거나 빈 문자열일 수 없습니다.");
+        if (this.participantNicknames != null) {
+            List<String> nicknames = Arrays.asList(this.participantNicknames.split(","));
+            this.participantNicknames = nicknames.stream()
+                    .map(nickname -> nickname.equals(oldNickname) ? newNickname : nickname)
+                    .collect(Collectors.joining(","));
+        }
+    }
+
+    public void removeParticipant(String nickname) {
+        if (!StringUtils.hasText(nickname))
+            throw new IllegalArgumentException("nickname은 null이거나 빈 문자열일 수 없습니다.");
+        if (this.participantNicknames != null) {
+            List<String> nicknames = new ArrayList<>(Arrays.asList(this.participantNicknames.split(",")));
+            nicknames.remove(nickname);
+            this.participantNicknames = String.join(",", nicknames);
+            this.participantCount = nicknames.size();
+        }
+    }
+
+    public void updateInvitationUrl(String invitationUrl) {
+        this.invitationUrl = invitationUrl;
+    }
+
 }
