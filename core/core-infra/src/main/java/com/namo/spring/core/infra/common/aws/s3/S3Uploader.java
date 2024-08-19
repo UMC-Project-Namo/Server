6abbb9a31897ae702708198bd2c45ca38f3f1ba9
hosung-222 <gik11kr@naver.com>
2024-08-19 23:14:00 +0900

    :sparkles: Feat: PreSignedUrl 생성 로직 구현
    <body>
    - PresignedURL 생성 로직
      - 유효기간 설정
      - 파일 이름 생성
      - 경로 생성
    
    - 관련 : #270

--- core/core-infra/src/main/java/com/namo/spring/core/infra/common/aws/s3/S3Uploader.java
+++ core/core-infra/src/main/java/com/namo/spring/core/infra/common/aws/s3/S3Uploader.java
@@ -1,12 +1,18 @@
 package com.namo.spring.core.infra.common.aws.s3;
 
 import java.io.InputStream;
+import java.net.URL;
+import java.util.Date;
+import java.util.UUID;
 
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.HttpMethod;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.Headers;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.DeleteObjectRequest;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.namo.spring.core.infra.config.AwsS3Config;
@@ -38,4 +44,75 @@ public class S3Uploader {
 		//Delete
 		amazonS3Client.deleteObject(deleteObjectRequest);
 	}
+
+	/**
+	 * presigned url 발급
+	 *
+	 * @param prefix   버킷 디렉토리 이름
+	 * @param fileName 클라이언트가 전달한 파일명 파라미터
+	 * @return presigned url
+	 */
+	public String getPreSignedUrl(String prefix, String fileName) {
+		if (!prefix.isEmpty()) {
+			fileName = createPath(prefix, fileName);
+		}
+
+		GeneratePresignedUrlRequest generatePresignedUrlRequest = getGeneratePreSignedUrlRequest(
+			awsS3Config.getBucketName(), fileName);
+		URL url = amazonS3Client.generatePresignedUrl(generatePresignedUrlRequest);
+		return url.toString();
+	}
+
+	/**
+	 * 파일 업로드용(PUT) presigned url 생성
+	 *
+	 * @param bucket   버킷 이름
+	 * @param fileName S3 업로드용 파일 이름
+	 * @return presigned url
+	 */
+	private GeneratePresignedUrlRequest getGeneratePreSignedUrlRequest(String bucket, String fileName) {
+		GeneratePresignedUrlRequest generatePresignedUrlRequest =
+			new GeneratePresignedUrlRequest(bucket, fileName)
+				.withMethod(HttpMethod.PUT)
+				.withExpiration(getPreSignedUrlExpiration());
+		generatePresignedUrlRequest.addRequestParameter(
+			Headers.S3_CANNED_ACL,
+			CannedAccessControlList.PublicRead.toString());
+		return generatePresignedUrlRequest;
+	}
+
+	/**
+	 * presigned url 유효 기간 설정
+	 *
+	 * @return 유효기간
+	 */
+	private Date getPreSignedUrlExpiration() {
+		Date expiration = new Date();
+		long expTimeMillis = expiration.getTime();
+		expTimeMillis += 1000 * 60 * 2;
+		expiration.setTime(expTimeMillis);
+		return expiration;
+	}
+
+	/**
+	 * 파일 고유 ID를 생성
+	 *
+	 * @return 36자리의 UUID
+	 */
+	private String createFileId() {
+		return UUID.randomUUID().toString();
+	}
+
+	/**
+	 * 파일의 전체 경로를 생성
+	 *
+	 * @param prefix 디렉토리 경로
+	 * @return 파일의 전체 경로
+	 */
+	private String createPath(String prefix, String fileName) {
+		String fileId = createFileId();
+		return String.format("%s/%s", prefix, fileId + fileName);
+	}
 }
+
+
