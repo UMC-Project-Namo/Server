4a7db4488df9df0e9196b3d9cbe5cdb494ceaab3
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-20 21:19:21 +0900

    :sparkles: Feat : 모임 생성 전 / 참여자 일정 조회 usecase 구현
    <footer>
    - 관련: #248

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/usecase/MeetingScheduleUsecase.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/usecase/MeetingScheduleUsecase.java
@@ -4,6 +4,7 @@ import com.namo.spring.application.external.api.schedule.dto.MeetingScheduleResp
 import com.namo.spring.application.external.api.schedule.dto.ScheduleRequest;
 import com.namo.spring.application.external.api.schedule.service.ScheduleManageService;
 import com.namo.spring.application.external.api.user.service.MemberManageService;
+import com.namo.spring.db.mysql.domains.schedule.dto.ScheduleParticipantQuery;
 import com.namo.spring.db.mysql.domains.schedule.entity.Schedule;
 import com.namo.spring.db.mysql.domains.user.entity.Member;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +16,11 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 import static com.namo.spring.application.external.api.schedule.converter.MeetingScheduleResponseConverter.toGetMeetingScheduleItemDtos;
-import static com.namo.spring.application.external.global.utils.MeetingValidationUtils.validateParticipantCount;
-import static com.namo.spring.application.external.global.utils.MeetingValidationUtils.validateParticipantIds;
+import static com.namo.spring.application.external.api.schedule.converter.MeetingScheduleResponseConverter.toGetMonthlyParticipantScheduleDtos;
+import static com.namo.spring.application.external.global.utils.MeetingParticipantValidationUtils.validateParticipantCount;
+import static com.namo.spring.application.external.global.utils.MeetingParticipantValidationUtils.validateParticipantIds;
+import static com.namo.spring.application.external.global.utils.SchedulePeriodValidationUtils.getExtendedPeriod;
+import static com.namo.spring.application.external.global.utils.SchedulePeriodValidationUtils.validateYearMonth;
 
 @Service
 @RequiredArgsConstructor
@@ -25,19 +29,28 @@ public class MeetingScheduleUsecase {
     private final ScheduleManageService scheduleManageService;
     private final MemberManageService memberManageService;
 
-    @Transactional(readOnly = true)
-    public List<MeetingScheduleResponse.GetMeetingScheduleItemDto> getMeetingSchedules(Long memberId) {
-        Member member = memberManageService.getMember(memberId);
-        return toGetMeetingScheduleItemDtos(scheduleManageService.getMeetingScheduleItemsByMember(member));
-    }
-
     @Transactional
     public Long createMeetingSchedule(ScheduleRequest.PostMeetingScheduleDto dto, MultipartFile image, Long memberId) {
-        validateParticipantIds(memberId, dto.getParticipants());
         validateParticipantCount(dto.getParticipants().size());
-
+        validateParticipantIds(memberId, dto.getParticipants());
         Member owner = memberManageService.getMember(memberId);
         Schedule schedule = scheduleManageService.createMeetingSchedule(dto, owner, image);
         return schedule.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<MeetingScheduleResponse.GetMeetingScheduleItemDto> getMeetingSchedules(Long memberId) {
+        Member member = memberManageService.getMember(memberId);
+        return toGetMeetingScheduleItemDtos(scheduleManageService.getMeetingScheduleItems(member));
+    }
+
+    public List<MeetingScheduleResponse.GetMonthlyParticipantScheduleDto> getMeetingParticiantsSchedules(List<Long> memberIds, int year, int month, Long memberId) {
+        validateYearMonth(year, month);
+        validateParticipantCount(memberIds.size());
+        validateParticipantIds(memberId, memberIds);
+
+        Member member = memberManageService.getMember(memberId);
+        List<ScheduleParticipantQuery> participantsWithSchedule = scheduleManageService.getMeetingParticipantSchedules(memberIds, getExtendedPeriod(year, month), null, member);
+        return toGetMonthlyParticipantScheduleDtos(participantsWithSchedule);
     }
 }
