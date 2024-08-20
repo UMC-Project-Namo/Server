e46ba74e8a2d5a80e7b8a770620be6407e7b8efb
hosung-222 <gik11kr@naver.com>
2024-08-20 12:07:21 +0900

    :sparkles: Feat: getPathForPrefix 구현
    <body>
    - 클라이언트로 부터 특정 값을 받아 buket 주소로 변환하는 메서드 구현
    
    - 관련 : #270

--- core/core-infra/src/main/java/com/namo/spring/core/infra/common/constant/FilePath.java
+++ core/core-infra/src/main/java/com/namo/spring/core/infra/common/constant/FilePath.java
@@ -4,14 +4,28 @@ import lombok.Getter;
 
 @Getter
 public enum FilePath {
-    MEETING_ACTIVITY_IMG("group/activity/"),
-    MEETING_PROFILE_IMG("group/profile/"),
-    INVITATION_ACTIVITY_IMG("invitation/activity/");
+	//v1
+	MEETING_ACTIVITY_IMG("", "group/activity/"),
+	MEETING_PROFILE_IMG("", "group/profile/"),
+	INVITATION_ACTIVITY_IMG("", "invitation/activity/"),
+	//v2
+	ACTIVITY_IMG("activity", "path/to/activity"),
+	DIARY_IMG("diary", "path/to/diary");;
 
-    private final String path;
+	private final String prefix;
+	private final String path;
 
-    FilePath(String path) {
-        this.path = path;
-    }
+	FilePath(String prefix, String path) {
+		this.prefix = prefix;
+		this.path = path;
+	}
 
+	public static String getPathForPrefix(String prefix) {
+		for (FilePath filePath : values()) {
+			if (filePath.prefix.equals(prefix)) {
+				return filePath.path;
+			}
+		}
+		throw new IllegalArgumentException("Invalid prefix: " + prefix);
+	}
 }
