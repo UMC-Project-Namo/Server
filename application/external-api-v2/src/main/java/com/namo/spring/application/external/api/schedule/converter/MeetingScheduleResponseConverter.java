7d3741d35f615238c2684bfa88fc579caff9d37a
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-08-21 14:44:02 +0900

    :sparkles: Feat : 모임 생성 후 / 참여자 일정 조회 response dto 구현
    <footer>
    - 관련: #248

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/converter/MeetingScheduleResponseConverter.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/converter/MeetingScheduleResponseConverter.java
@@ -8,7 +8,11 @@ import com.namo.spring.db.mysql.domains.schedule.entity.Schedule;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.namo.spring.db.mysql.domains.category.type.PaletteEnum.getPaletteColorIds;
 
 public class MeetingScheduleResponseConverter {
     private MeetingScheduleResponseConverter() {
@@ -32,23 +36,28 @@ public class MeetingScheduleResponseConverter {
                 .build();
     }
 
-    public static List<MeetingScheduleResponse.GetMonthlyParticipantScheduleDto> toGetMonthlyParticipantScheduleDtos(List<ScheduleParticipantQuery> participantsWithSchedule) {
+    public static List<MeetingScheduleResponse.GetMonthlyParticipantScheduleDto> toGetMonthlyParticipantScheduleDtos(List<ScheduleParticipantQuery> participantsWithSchedule, List<Long> participantIds, Long ownerId) {
+        long[] palette = getPaletteColorIds();
         Map<Long, List<ScheduleParticipantQuery>> scheduleIdAndParticipant
                 = participantsWithSchedule.stream()
                 .collect(Collectors.groupingBy(participant -> participant.getSchedule().getId()));
+        Map<Long, Long> participantAndPalette
+                = IntStream.range(0, participantIds.size()).boxed()
+                .collect(Collectors.toMap(participantIds::get, i -> palette[i + 1]));
+        participantAndPalette.put(ownerId, palette[0]);
         return scheduleIdAndParticipant.entrySet().stream()
-                .map(entry -> toGetMonthlyParticipantScheduleDto(entry.getValue().get(0).getSchedule(), entry.getValue()))
+                .map(entry -> toGetMonthlyParticipantScheduleDto(entry.getValue().get(0).getSchedule(), entry.getValue(), participantAndPalette))
                 .collect(Collectors.toList());
     }
 
-    public static MeetingScheduleResponse.GetMonthlyParticipantScheduleDto toGetMonthlyParticipantScheduleDto(Schedule schedule, List<ScheduleParticipantQuery> participant) {
+    public static MeetingScheduleResponse.GetMonthlyParticipantScheduleDto toGetMonthlyParticipantScheduleDto(Schedule schedule, List<ScheduleParticipantQuery> participant, Map<Long, Long> participantAndPalette) {
         return MeetingScheduleResponse.GetMonthlyParticipantScheduleDto.builder()
                 .scheduleId(schedule.getId())
                 .name(schedule.getTitle())
                 .startDate(DateUtil.toSeconds(schedule.getPeriod().getStartDate()))
                 .endDate(DateUtil.toSeconds(schedule.getPeriod().getEndDate()))
                 .interval(schedule.getPeriod().getDayInterval())
-                .participants(toParticipantDtos(participant))
+                .participants(toParticipantDtos(participant, participantAndPalette))
                 .latitude(schedule.getLocation().getLatitude())
                 .longitude(schedule.getLocation().getLongitude())
                 .locationName(schedule.getLocation().getName())
@@ -62,10 +71,51 @@ public class MeetingScheduleResponseConverter {
                 .collect(Collectors.toList());
     }
 
+    private static List<MeetingScheduleResponse.ParticipantDto> toParticipantDtos(List<ScheduleParticipantQuery> participant, Map<Long, Long> participantAndPalette) {
+        return participant.stream()
+                .map(p -> toParticipantDto(p, participantAndPalette))
+                .collect(Collectors.toList());
+    }
+
     private static MeetingScheduleResponse.ParticipantDto toParticipantDto(ScheduleParticipantQuery participant) {
         return MeetingScheduleResponse.ParticipantDto.builder()
                 .memberId(participant.getMemberId())
                 .nickname(participant.getNickname())
+                .color(participant.getParticipantPaletteId())
+                .build();
+    }
+
+    private static MeetingScheduleResponse.ParticipantDto toParticipantDto(ScheduleParticipantQuery participant, Map<Long, Long> participantAndPalette) {
+        return MeetingScheduleResponse.ParticipantDto.builder()
+                .memberId(participant.getMemberId())
+                .nickname(participant.getNickname())
+                .color(participantAndPalette.get(participant.getMemberId()))
+                .build();
+    }
+
+
+    public static List<MeetingScheduleResponse.GetMonthlyMeetingParticipantScheduleDto> toGetMonthlyMeetingParticipantScheduleDtos(List<ScheduleParticipantQuery> participantsWithSchedule, Long curScheduleId) {
+        Map<Long, List<ScheduleParticipantQuery>> scheduleIdAndParticipant
+                = participantsWithSchedule.stream()
+                .collect(Collectors.groupingBy(participant -> participant.getSchedule().getId()));
+        return scheduleIdAndParticipant.entrySet().stream()
+                .map(entry -> toGetMonthlyMeetingParticipantScheduleDto(entry.getValue().get(0).getSchedule(), entry.getValue(), Objects.equals(entry.getKey(), curScheduleId)))
+                .collect(Collectors.toList());
+    }
+
+    public static MeetingScheduleResponse.GetMonthlyMeetingParticipantScheduleDto toGetMonthlyMeetingParticipantScheduleDto(Schedule schedule, List<ScheduleParticipantQuery> participant, boolean isCurMeetingSchedule) {
+        return MeetingScheduleResponse.GetMonthlyMeetingParticipantScheduleDto.builder()
+                .scheduleId(schedule.getId())
+                .name(schedule.getTitle())
+                .startDate(DateUtil.toSeconds(schedule.getPeriod().getStartDate()))
+                .endDate(DateUtil.toSeconds(schedule.getPeriod().getEndDate()))
+                .interval(schedule.getPeriod().getDayInterval())
+                .participants(toParticipantDtos(participant))
+                .isCurMeetingSchedule(isCurMeetingSchedule)
+                .latitude(schedule.getLocation().getLatitude())
+                .longitude(schedule.getLocation().getLongitude())
+                .locationName(schedule.getLocation().getName())
+                .kakaoLocationId(schedule.getLocation().getKakaoLocationId())
                 .build();
     }
 
--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/dto/MeetingScheduleResponse.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/dto/MeetingScheduleResponse.java
@@ -36,7 +36,7 @@ public class MeetingScheduleResponse {
     @Builder
     @Schema(title = "모임 생성 전 / 월간 일정 조회 응답 DTO")
     public static class GetMonthlyParticipantScheduleDto {
-        @Schema(description = "모임 일정 ID")
+        @Schema(description = "일정 ID")
         private Long scheduleId;
         @Schema(description = "일정 이름", example = "나모 정기 회의")
         private String name;
@@ -46,7 +46,7 @@ public class MeetingScheduleResponse {
         private Long endDate;
         @Schema(description = "시작일과 종료일 차이")
         private Long interval;
-        @Schema(description = "모임 일정 참여 유저 목록")
+        @Schema(description = "일정 참여자 목록")
         private List<ParticipantDto> participants;
         @Schema(description = "장소 위치 경도")
         private Double longitude;
@@ -56,8 +56,35 @@ public class MeetingScheduleResponse {
         private String locationName;
         @Schema(description = "장소 카카오 맵 ID")
         private String kakaoLocationId;
-        @Schema(description = "모임 일정에 대한 기록이 존재하는지 여부")
-        private boolean hasDiaryPlace;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    @Schema(title = "모임 생성 후 / 월간 일정 조회 응답 DTO")
+    public static class GetMonthlyMeetingParticipantScheduleDto {
+        @Schema(description = "일정 ID")
+        private Long scheduleId;
+        @Schema(description = "일정 이름", example = "나모 정기 회의")
+        private String name;
+        @Schema(description = "일정 시작일, unix 타임스탬프 형식")
+        private Long startDate;
+        @Schema(description = "일정 종료일, unix 타임스탬프 형식")
+        private Long endDate;
+        @Schema(description = "시작일과 종료일 차이")
+        private Long interval;
+        @Schema(description = "일정 참여자 목록")
+        private List<ParticipantDto> participants;
+        @Schema(description = "현재 조회하는 모임 일정인지의 여부")
+        private boolean isCurMeetingSchedule = false;
+        @Schema(description = "장소 위치 경도")
+        private Double longitude;
+        @Schema(description = "장소 위치 위도")
+        private Double latitude;
+        @Schema(description = "장소 이름", name = "스타벅스 강남역점")
+        private String locationName;
+        @Schema(description = "장소 카카오 맵 ID")
+        private String kakaoLocationId;
     }
 
     @AllArgsConstructor
@@ -70,6 +97,6 @@ public class MeetingScheduleResponse {
         @Schema(description = "닉네임")
         private String nickname;
         @Schema(description = "색상")
-        private Integer color;
+        private Long color;
     }
 }
