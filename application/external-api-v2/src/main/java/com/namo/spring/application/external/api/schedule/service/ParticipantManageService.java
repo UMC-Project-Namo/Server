7a4a2cda9f1c28aceac99db898d16006b307d2d2
hosung-222 <gik11kr@naver.com>
2024-08-29 17:42:47 +0900

    :bug: Fix: 충돌 해결
    <body>
    - git 충돌해결
    
    - 관련 : #270

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/service/ParticipantManageService.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/service/ParticipantManageService.java
@@ -59,79 +59,9 @@ public class ParticipantManageService {
 			return participants;
 	}
 
-	// =======
-	// 	private static final Long MEETING_SCHEDULE_OWNER_PALETTE_ID = ColorChip.getMeetingScheduleOwnerPaletteId();
-	// 	private final ParticipantMaker participantMaker;
-	// 	private final ParticipantService participantService;
-	// 	private final MemberService memberService;
-	// 	private final CategoryService categoryService;
-	// 	private final PaletteService paletteService;
-	//
-
-	//
-	// 	@Transactional(readOnly = true)
-	// 	public List<Member> getValidatedMeetingParticipants(List<Long> memberIds) {
-	// 		validateParticipantNumber(memberIds.size());
-	// 		List<Member> participants = memberService.readMembersById(memberIds);
-	// 		if (participants.size() != memberIds.size()) {
-	// 			throw new MemberException(ErrorStatus.NOT_FOUND_USER_FAILURE);
-	// 		} else
-	// 			return participants;
-	// 	}
-	//
-	// 	@Transactional
-	// 	public void createPersonalScheduleParticipant(Member member, Schedule schedule, Long categoryId) {
-	// 		Category category = categoryService.readCategoryByMemberAndId(categoryId, member);
-	// 		participantMaker.makeScheduleOwner(schedule, member, category, null);
-	// 	}
-	//
-	// 	@Transactional
-	// 	public void createMeetingScheduleParticipants(Member scheduleOwner, Schedule schedule, List<Member> participants) {
-	// 		Category category = categoryService.readMeetingCategoryByMember(scheduleOwner);
-	// 		Palette palette = paletteService.getPalette(MEETING_SCHEDULE_OWNER_PALETTE_ID);
-	// 		participantMaker.makeScheduleOwner(schedule, scheduleOwner, category, palette);
-	//
-	// 		participants.forEach(participant -> participantMaker.makeMeetingScheduleParticipant(schedule, participant));
-	// 	}
-	// >>>>>>> 735748e8 (:sparkles: Feat: 다이어리 작성을 위한 Participant 조회 메서드 구현 - getParticipantForDiary())
-
 	public Participant getScheduleParticipant(Long memberId, Long scheduleId) {
 		return participantService.readParticipants(memberId, scheduleId)
 			.orElseThrow(() -> new MemberException(ErrorStatus.NOT_FOUND_PARTICIPANT_FAILURE));
 	}
 
-	// =======
-	//
-	// 	public Diary getParticipantDiary(Participant participant) {
-	// 		if (!participant.isHasDiary())
-	// 			throw new MemberException(ErrorStatus.NOT_WRITTEN_DIARY_FAILURE);
-	// 		//Todo: 이후 다이어리를 여러 버전으로 저장하는 기획 변경시 최신 버전을 가져오도록 수정 필요 (현재는 첫번째 다이어리만 가져옴) - 2024.8.25
-	// 		return participant.getDiaries().get(0);
-	// 	}
-	//
-	// 	@Transactional(readOnly = true)
-	// 	public List<Member> getValidatedMeetingParticipants(List<Long> memberIds) {
-	// 		validateParticipantNumber(memberIds.size());
-	// 		List<Member> participants = memberService.readMembersById(memberIds);
-	// 		if (participants.size() != memberIds.size()) {
-	// 			throw new MemberException(ErrorStatus.NOT_FOUND_USER_FAILURE);
-	// 		} else
-	// 			return participants;
-	// 	}
-	//
-	// 	@Transactional
-	// 	public void createPersonalScheduleParticipant(Member member, Schedule schedule, Long categoryId) {
-	// 		Category category = categoryService.readCategoryByMemberAndId(categoryId, member);
-	// 		participantMaker.makeScheduleOwner(schedule, member, category, null);
-	// 	}
-	//
-	// 	@Transactional
-	// 	public void createMeetingScheduleParticipants(Member scheduleOwner, Schedule schedule, List<Member> participants) {
-	// 		Category category = categoryService.readMeetingCategoryByMember(scheduleOwner);
-	// 		Palette palette = paletteService.getPalette(MEETING_SCHEDULE_OWNER_PALETTE_ID);
-	// 		participantMaker.makeScheduleOwner(schedule, scheduleOwner, category, palette);
-	//
-	// 		participants.forEach(participant -> participantMaker.makeMeetingScheduleParticipant(schedule, participant));
-	// 	}
-	// >>>>>>> 4f19f562 (:sparkles: Feat: 스케쥴 참여자의 일기를 가져오는 메서드 getParticipantDiary() 구현)
 }
