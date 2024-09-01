820680ecd8eb0912f3e28018fed7e4e99c16aac4
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-09-01 11:24:25 +0900

    :sparkles: Feat : 일정 fk , 알림 시각 컬럼 추가
    <footer>
    - 관련: #278

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/notification/entity/Notification.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/notification/entity/Notification.java
@@ -1,32 +1,22 @@
 package com.namo.spring.db.mysql.domains.notification.entity;
 
-import java.time.LocalDateTime;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-
-import org.hibernate.annotations.DynamicInsert;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
-
 import com.namo.spring.db.mysql.common.converter.NotificationTypeConverter;
 import com.namo.spring.db.mysql.common.converter.PublisherTypeConverter;
 import com.namo.spring.db.mysql.common.model.BaseTimeEntity;
 import com.namo.spring.db.mysql.domains.notification.type.NotificationType;
 import com.namo.spring.db.mysql.domains.notification.type.PublisherType;
+import com.namo.spring.db.mysql.domains.schedule.entity.Schedule;
 import com.namo.spring.db.mysql.domains.user.entity.Member;
-
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
@@ -34,51 +24,61 @@ import lombok.NoArgsConstructor;
 @DynamicInsert
 public class Notification extends BaseTimeEntity {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@JdbcTypeCode(SqlTypes.VARCHAR)
-	@Convert(converter = PublisherTypeConverter.class)
-	@Column(nullable = false, length = 50)
-	private PublisherType publisherType;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Convert(converter = PublisherTypeConverter.class)
+    @Column(nullable = false, length = 50)
+    private PublisherType publisherType;
 
-	@JdbcTypeCode(SqlTypes.VARCHAR)
-	@Column(nullable = false, length = 20)
-	private String publisherName;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Column(nullable = false, length = 20)
+    private String publisherName;
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "receiver_id", nullable = false)
-	private Member receiver;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "receiver_id", nullable = false)
+    private Member receiver;
 
-	@ManyToOne(fetch = FetchType.LAZY, optional = false)
-	@JoinColumn(name = "device_id", nullable = false)
-	private Device device;
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "device_id", nullable = false)
+    private Device device;
 
-	@JdbcTypeCode(SqlTypes.VARCHAR)
-	@Convert(converter = NotificationTypeConverter.class)
-	@Column(nullable = false, length = 50)
-	private NotificationType notificationType;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id")
+    private Schedule schedule;
 
-	@Column(nullable = false)
-	private String notificationJson;
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    @Convert(converter = NotificationTypeConverter.class)
+    @Column(nullable = false, length = 50)
+    private NotificationType notificationType;
 
-	@Column(nullable = false)
-	private boolean isRead;
+    @Column(nullable = false)
+    private String notificationJson;
 
-	private LocalDateTime readAt;
+    @Column(nullable = false)
+    private boolean isRead;
 
-	private boolean isDeleted;
+    // 일정 시작 알림일 때 설정
+    private LocalDateTime notifyAt;
 
-	@Builder
-	public Notification(PublisherType publisherType, String publisherName, Member receiver,
-		Device device, NotificationType notificationType, String notificationJson) {
-		this.publisherType = publisherType;
-		this.publisherName = publisherName;
-		this.receiver = receiver;
-		this.notificationType = notificationType;
-		this.notificationJson = notificationJson;
-		this.isRead = false;
-		this.isDeleted = false;
-	}
+    private LocalDateTime readAt;
+
+    private boolean isDeleted;
+
+    @Builder
+    public Notification(PublisherType publisherType, String publisherName, Member receiver,
+                        Device device, Schedule schedule, LocalDateTime notifyAt, NotificationType notificationType, String notificationJson) {
+        this.publisherType = publisherType;
+        this.publisherName = publisherName;
+        this.receiver = receiver;
+        this.device = device;
+        this.schedule = schedule;
+        this.notificationType = notificationType;
+        this.notificationJson = notificationJson;
+        this.notifyAt = notifyAt;
+        this.isRead = false;
+        this.isDeleted = false;
+    }
 }
