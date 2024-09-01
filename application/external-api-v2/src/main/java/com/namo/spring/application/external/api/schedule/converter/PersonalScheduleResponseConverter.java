b0a66d9103eb219c3ce17cdc953e1b76395bf3e8
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-09-01 10:53:19 +0900

    :sparkles: Feat : 개인 월간 일정 조회 dto 및 converter 구현
    <footer>
    - 관련: #278

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/converter/PersonalScheduleResponseConverter.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/schedule/converter/PersonalScheduleResponseConverter.java
@@ -1,8 +1,100 @@
 package com.namo.spring.application.external.api.schedule.converter;
 
+import com.namo.spring.application.external.api.schedule.dto.PersonalScheduleResponse;
+import com.namo.spring.core.common.utils.DateUtil;
+import com.namo.spring.db.mysql.domains.category.entity.Category;
+import com.namo.spring.db.mysql.domains.notification.dto.ScheduleNotificationQuery;
+import com.namo.spring.db.mysql.domains.schedule.entity.Participant;
+import com.namo.spring.db.mysql.domains.schedule.entity.Schedule;
+import com.namo.spring.db.mysql.domains.schedule.type.Location;
+import com.namo.spring.db.mysql.domains.schedule.type.ScheduleType;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class PersonalScheduleResponseConverter {
     private PersonalScheduleResponseConverter() {
         throw new IllegalStateException("Util Class");
     }
 
+    public static List<PersonalScheduleResponse.GetMonthlyScheduleDto> toGetMonthlyScheduleDtos(List<Participant> participants, List<ScheduleNotificationQuery> notifications) {
+        Map<Long, List<ScheduleNotificationQuery>> scheduleIdAndNotification = notifications.stream()
+                .collect(Collectors.groupingBy(ScheduleNotificationQuery::getScheduleId));
+
+        return participants.stream()
+                .map(participant -> {
+                    Long scheduleId = participant.getSchedule().getId();
+                    List<ScheduleNotificationQuery> notificationList = scheduleIdAndNotification.getOrDefault(scheduleId, null);
+                    return toGetMonthlyScheduleDto(participant, notificationList);
+                })
+                .collect(Collectors.toList());
+    }
+
+    public static PersonalScheduleResponse.GetMonthlyScheduleDto toGetMonthlyScheduleDto(Participant participant, List<ScheduleNotificationQuery> notifications) {
+        PersonalScheduleResponse.GetMonthlyScheduleDto.GetMonthlyScheduleDtoBuilder builder = PersonalScheduleResponse.GetMonthlyScheduleDto.builder()
+                .scheduleId(participant.getSchedule().getId())
+                .title(participant.getSchedule().getTitle())
+                .category(toCategoryDto(participant.getCategory()))
+                .startDate(DateUtil.toSeconds(participant.getSchedule().getPeriod().getStartDate()))
+                .endDate(DateUtil.toSeconds(participant.getSchedule().getPeriod().getEndDate()))
+                .interval(participant.getSchedule().getPeriod().getDayInterval())
+                .location(participant.getSchedule().getLocation() != null ? toLocationDto(participant.getSchedule().getLocation()) : null)
+                .isMeetingSchedule(getIsMeetingSchedule(participant.getSchedule().getScheduleType()))
+                .hasDiary(participant.isHasDiary())
+                .notification(notifications != null ? toNotificationDtos(notifications) : null);
+        if (getIsMeetingSchedule(participant.getSchedule().getScheduleType())) {
+            return builder
+                    .meetingInfo(toMeetingInfoDto(participant.getIsOwner(), participant.getSchedule()))
+                    .build();
+        } else return builder.build();
+    }
+
+    private static PersonalScheduleResponse.CategoryDto toCategoryDto(Category category) {
+        return PersonalScheduleResponse.CategoryDto.builder()
+                .categoryId(category.getId())
+                .colorId(category.getPalette().getId())
+                .name(category.getName())
+                .build();
+    }
+
+    private static PersonalScheduleResponse.LocationDto toLocationDto(Location location) {
+        return PersonalScheduleResponse.LocationDto.builder()
+                .longitude(location.getLongitude())
+                .latitude(location.getLatitude())
+                .kakaoLocationId(location.getKakaoLocationId())
+                .locationName(location.getName())
+                .build();
+    }
+
+    private static PersonalScheduleResponse.MeetingInfoDto toMeetingInfoDto(Integer isOwner, Schedule schedule) {
+        return PersonalScheduleResponse.MeetingInfoDto.builder()
+                .participantNicknames(schedule.getParticipantNicknames())
+                .participantCount(schedule.getParticipantCount())
+                .isOwner(getParticipantIsOwner(isOwner))
+                .build();
+    }
+
+    private static List<PersonalScheduleResponse.NotificationDto> toNotificationDtos(List<ScheduleNotificationQuery> notifications) {
+        return notifications.stream()
+                .sorted(Comparator.comparing(ScheduleNotificationQuery::getNotifyAt).reversed())
+                .map(PersonalScheduleResponseConverter::toNotificationDto)
+                .collect(Collectors.toList());
+    }
+
+    private static PersonalScheduleResponse.NotificationDto toNotificationDto(ScheduleNotificationQuery notification) {
+        return PersonalScheduleResponse.NotificationDto.builder()
+                .notificationId(notification.getNotificationId())
+                .notifyDate(DateUtil.toSeconds(notification.getNotifyAt()))
+                .build();
+    }
+
+    private static boolean getIsMeetingSchedule(int type) {
+        return type == ScheduleType.MEETING.getValue();
+    }
+
+    private static boolean getParticipantIsOwner(int isOwner) {
+        return isOwner == ScheduleType.MEETING.getValue();
+    }
 }
