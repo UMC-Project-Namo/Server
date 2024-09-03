4e3f733807fee9eb8ecd037f6ca0c954eece18e4
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-09-03 09:47:43 +0900

    :sparkles: Feat : 두 유저가 친구인지 조회하는 메서드 구현
    <footer>
    - 관련: #278

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/repository/FriendshipRepository.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/repository/FriendshipRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
     @Query("SELECT DISTINCT f FROM Friendship f JOIN FETCH f.friend WHERE f.member.id = :memberId AND f.friend.id in :members AND f.status = 'ACCEPTED' AND f.member.status = '2'")
     List<Friendship> findAcceptedFriendshipsByMemberIdAndFriendIds(Long memberId, List<Long> members);
+
+    boolean existsByMemberIdAndFriendId(Long memberId, Long friendId);
 }
