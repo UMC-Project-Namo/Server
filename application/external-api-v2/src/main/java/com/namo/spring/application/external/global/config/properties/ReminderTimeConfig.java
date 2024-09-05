1241c9c6a939910597b7c35007010f13afc74f6a
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-09-05 20:37:13 +0900

    :memo: Docs : 주석 작성
    <footer>
    - 관련: #282

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/global/config/properties/ReminderTimeConfig.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/global/config/properties/ReminderTimeConfig.java
@@ -10,7 +10,25 @@ import java.util.regex.Pattern;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReminderTimeConfig {
+
+    /**
+     * 알림 메세지를 위해 시간을 "HH:mm" 형식으로 포맷팅하기 위한 DateTimeFormatter입니다.
+     * 예: "14:30"
+     */
     public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
+    /**
+     * 정시 알림을 나타내는 트리거 값으로, 고정 값입니다.
+     * 이 값은 다른 시간 트리거(예: "M30", "H1" 등)과 구분하기 위해 사용됩니다.
+     */
     public static final String SCHEDULED_TIME_FIXED_VALUE = "ST";
+
+    /**
+     * 유효한 알림 시간 패턴을 정의하는 정규 표현식입니다.
+     * 허용되는 형식:
+     * - M1 ~ M59: 1분에서 59분 전
+     * - H1 ~ H36: 1시간에서 36시간 전
+     * - D1 ~ D7: 1일에서 7일 전
+     */
     public static final Pattern NOTIFICATION_TIME_PATTERN = Pattern.compile("^(M[1-9]|M[1-5][0-9]|H([1-9]|[1-2][0-9]|3[0-6])|D[1-7])$");
 }
