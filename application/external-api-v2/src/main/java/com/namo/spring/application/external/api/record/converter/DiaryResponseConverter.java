44fdc568bded58e1fadda7ea07770c146eaf4391
hosung-222 <gik11kr@naver.com>
2024-09-05 19:07:57 +0900

    :sparkles: Feature: DayOfDiaryDto 컨버터 구현
    <body>
    - 날짜별 일기 존재하는 스케줄 조회 컨버터 구현
    
    - 관련 : #283

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/record/converter/DiaryResponseConverter.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/record/converter/DiaryResponseConverter.java
@@ -1,12 +1,15 @@
 package com.namo.spring.application.external.api.record.converter;
 
+import java.time.YearMonth;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import com.namo.spring.application.external.api.record.dto.DiaryResponse;
+import com.namo.spring.db.mysql.domains.category.entity.Category;
 import com.namo.spring.db.mysql.domains.record.entity.Diary;
 import com.namo.spring.db.mysql.domains.record.entity.DiaryImage;
 import com.namo.spring.db.mysql.domains.schedule.entity.Participant;
+import com.namo.spring.db.mysql.domains.schedule.entity.Schedule;
 
 public class DiaryResponseConverter {
 
@@ -32,12 +35,26 @@ public class DiaryResponseConverter {
 	public static DiaryResponse.DiaryArchiveDto toDiaryArchiveDto(Participant participant) {
 		return DiaryResponse.DiaryArchiveDto.builder()
 			.scheduleDate(participant.getSchedule().getPeriod().getStartDate())
+			.categoryInfo(toCategoryInfoDto(participant.getCategory()))
 			.scheduleId(participant.getSchedule().getId())
 			.title(participant.getSchedule().getTitle())
 			.diarySummary(toDiarySummaryDto(participant.getDiary()))
 			.scheduleType(participant.getSchedule().getScheduleType())
-			.participantsCount(participant.getSchedule().getParticipantCount())
-			.participantsNames(participant.getSchedule().getParticipantNicknames())
+			.participantInfo(toParticipantInfo(participant.getSchedule()))
+			.build();
+	}
+
+	private static DiaryResponse.CategoryInfoDto toCategoryInfoDto(Category category) {
+		return DiaryResponse.CategoryInfoDto.builder()
+			.name(category.getName())
+			.color(category.getPalette().getColor())
+			.build();
+	}
+
+	private static DiaryResponse.ParticipantInfo toParticipantInfo(Schedule schedule) {
+		return DiaryResponse.ParticipantInfo.builder()
+			.participantsCount(schedule.getParticipantCount())
+			.participantsNames(schedule.getParticipantNicknames())
 			.build();
 	}
 
@@ -51,16 +68,26 @@ public class DiaryResponseConverter {
 			.build();
 	}
 
-	public static DiaryResponse.DiaryExistDateDto toDiaryExistDateDto(List<Participant> participants, int year,
-		int month) {
+	public static DiaryResponse.DiaryExistDateDto toDiaryExistDateDto(List<Participant> participants,
+		YearMonth yearMonth) {
 		return DiaryResponse.DiaryExistDateDto.builder()
-			.year(year)
-			.month(month)
+			.year(yearMonth.getYear())
+			.month(yearMonth.getMonth().getValue())
 			.dates(participants.stream()
 				.map(participant -> participant.getSchedule().getPeriod().getStartDate().getDayOfMonth()) // 날짜만 추출
 				.distinct() // 중복 제거 (만약 중복된 날짜가 있을 경우)
 				.sorted()
 				.collect(Collectors.toList()))
+			.build();
+	}
+
+	public static DiaryResponse.DayOfDiaryDto toDayOfDiaryDto(Participant participant) {
+		return DiaryResponse.DayOfDiaryDto.builder()
+			.scheduleType(participant.getSchedule().getScheduleType())
+			.categoryInfo(toCategoryInfoDto(participant.getCategory()))
+			.scheduleDate(participant.getSchedule().getPeriod().getStartDate())
+			.scheduleTitle(participant.getSchedule().getTitle())
+			.participantInfo(toParticipantInfo(participant.getSchedule()))
 			.build();
 	}
 }
