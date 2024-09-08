4862ec5e324f014c1e991786f0747d3b865b9fa9
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-09-08 12:36:24 +0900

    :sparkles: Feat : fcm request json map 형식에서 dto 형식으로 변경
    <footer>
    - 관련: #282

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/notification/service/FcmJsonMessageBuilder.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/notification/service/FcmJsonMessageBuilder.java
@@ -2,11 +2,14 @@ package com.namo.spring.application.external.api.notification.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.namo.spring.application.external.api.notification.dto.FcmNotificationRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
+
+import static com.namo.spring.application.external.api.notification.dto.FcmNotificationRequest.toAndroidFcmMessageDto;
+import static com.namo.spring.application.external.api.notification.dto.FcmNotificationRequest.toIosFcmMessageDto;
 
 @Component
 @RequiredArgsConstructor
@@ -15,51 +18,15 @@ public class FcmJsonMessageBuilder {
     private final ObjectMapper objectMapper;
 
     public String buildAndroidFcmMessage(String token, String title, String body, Map<String, String> data) throws JsonProcessingException {
-        Map<String, Object> message = new HashMap<>();
-        message.put("token", token);
-
-        Map<String, String> notification = new HashMap<>();
-        notification.put("title", title);
-        notification.put("body", body);
-        message.put("notification", notification);
-
-        if (data != null && !data.isEmpty()) {
-            message.put("data", data);
-        }
-
-        Map<String, Object> fcmMessage = new HashMap<>();
-        fcmMessage.put("message", message);
+        FcmNotificationRequest.AndroidFcmMessageDto fcmMessage
+                = toAndroidFcmMessageDto(token, title, body, data);
 
         return objectMapper.writeValueAsString(fcmMessage);
     }
 
-    public String buildIOSFcmMessage(String token, String title, String body, Map<String, Object> data, int badge) throws JsonProcessingException {
-        Map<String, Object> message = new HashMap<>();
-        message.put("token", token);
-
-        Map<String, Object> notification = new HashMap<>();
-        notification.put("title", title);
-        notification.put("body", body);
-        message.put("notification", notification);
-
-        Map<String, Object> apns = new HashMap<>();
-        Map<String, Object> apnsPayload = new HashMap<>();
-        Map<String, Object> aps = new HashMap<>();
-
-        aps.put("alert", notification);
-        aps.put("badge", badge);
-        aps.put("sound", "default");
-
-        apnsPayload.put("aps", aps);
-        if (data != null && !data.isEmpty()) {
-            apnsPayload.putAll(data);
-        }
-
-        apns.put("payload", apnsPayload);
-        message.put("apns", apns);
-
-        Map<String, Object> fcmMessage = new HashMap<>();
-        fcmMessage.put("message", message);
+    public String buildIOSFcmMessage(String token, String title, String body, Map<String, String> data, int badge) throws JsonProcessingException {
+        FcmNotificationRequest.IosFcmMessageDto fcmMessage
+                = toIosFcmMessageDto(token, title, body, data);
 
         return objectMapper.writeValueAsString(fcmMessage);
     }
