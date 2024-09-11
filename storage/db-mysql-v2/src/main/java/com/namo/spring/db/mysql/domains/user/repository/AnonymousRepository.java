cb197cd927cee24a36852ab47d164511c00561d2
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-09-11 21:51:35 +0900

    :sparkles: Feat : 모든 참여 코드를 가져오는 메서드 및 참여 코드로 비회원을 가져오는 메서드 구현
    <footer>
    - 관련: #272

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/repository/AnonymousRepository.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/repository/AnonymousRepository.java
@@ -1,11 +1,19 @@
 package com.namo.spring.db.mysql.domains.user.repository;
 
+import com.namo.spring.db.mysql.domains.user.dto.AnonymousInviteCodeQuery;
 import com.namo.spring.db.mysql.domains.user.entity.Anonymous;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AnonymousRepository extends JpaRepository<Anonymous, Long> {
 
     List<Anonymous> findAnonymousByNickname(String nickname);
+
+    Optional<Anonymous> findAnonymousByInviteCode(String inviteCode);
+
+    @Query("SELECT new com.namo.spring.db.mysql.domains.user.dto.AnonymousInviteCodeQuery(a.inviteCode) FROM Anonymous a")
+    List<AnonymousInviteCodeQuery> findAllInviteCodes();
 }
--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/service/AnonymousService.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/user/service/AnonymousService.java
@@ -1,11 +1,13 @@
 package com.namo.spring.db.mysql.domains.user.service;
 
 import com.namo.spring.core.common.annotation.DomainService;
+import com.namo.spring.db.mysql.domains.user.dto.AnonymousInviteCodeQuery;
 import com.namo.spring.db.mysql.domains.user.entity.Anonymous;
 import com.namo.spring.db.mysql.domains.user.repository.AnonymousRepository;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
+import java.util.Optional;
 
 @DomainService
 @RequiredArgsConstructor
@@ -13,10 +15,18 @@ public class AnonymousService {
     private final AnonymousRepository anonymousRepository;
 
     public Anonymous createAnonymous(Anonymous anonymous) {
-        anonymousRepository.save(anonymous);
+        return anonymousRepository.save(anonymous);
     }
 
     public List<Anonymous> findAnonymousNickname(String nickname) {
         return anonymousRepository.findAnonymousByNickname(nickname);
+    }
+
+    public Optional<Anonymous> findAnonymousByInviteCode(String code) {
+        return anonymousRepository.findAnonymousByInviteCode(code);
+    }
+
+    public List<AnonymousInviteCodeQuery> findAllInviteCodes() {
+        return anonymousRepository.findAllInviteCodes();
     }
 }
