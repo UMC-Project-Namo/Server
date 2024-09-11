af28f39f1bfa394530c2c22b61d3503df52ef0c1
SEO JOO WON <85955988+joowojr@users.noreply.github.com>
2024-09-11 19:53:47 +0900

    :fire: Remove : InvitationCode redis db 삭제
    <footer>
    - 관련: #272

--- storage/db-redis/src/main/java/com/namo/spring/db/redis/cache/code/InvitationCodeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.namo.spring.db.redis.cache.code;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface InvitationCodeRepository extends CrudRepository<InvitationCode, String> {
-}
