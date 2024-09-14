6e1d2d2f8f60a69ad0c441219862d5e0dc95b61d
hosung-222 <gik11kr@naver.com>
2024-09-15 00:05:42 +0900

    :sparkles: Feat: 카테고리 조회 API 구현완료
    <body>
    API 구현완료 + 스웨거 명세
    
    - 관련 : #307

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/category/controller/CategoryController.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/category/controller/CategoryController.java
@@ -8,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.namo.spring.application.external.api.category.dto.CategoryResponse;
+import com.namo.spring.application.external.api.category.usecase.CategoryUseCase;
+import com.namo.spring.application.external.global.annotation.swagger.ApiErrorCodes;
 import com.namo.spring.application.external.global.common.security.authentication.SecurityUserDetails;
+import com.namo.spring.core.common.code.status.ErrorStatus;
 import com.namo.spring.core.common.response.ResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.RequiredArgsConstructor;
@@ -21,11 +25,15 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v2/categories")
 public class CategoryController {
 
+    private final CategoryUseCase categoryUseCase;
+
+    @Operation(summary = "나의 카테고리 목록 조회", description = "나의 카테고리 목록이 조회됩니다. 이후 뎁스의 내용도 포함됩니다.")
     @GetMapping("")
     public ResponseDto<List<CategoryResponse.MyCategoryInfoDto>> getCategories(
             @AuthenticationPrincipal SecurityUserDetails memberInfo
     ){
-        return ResponseDto.onSuccess(null);
+        return ResponseDto.onSuccess(categoryUseCase
+                .getMyCategoryList(memberInfo.getUserId()));
     }
 
 }
