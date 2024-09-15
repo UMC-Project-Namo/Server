1c527ad4299358edfbffc072ad7e25390ab70f09
hosung-222 <gik11kr@naver.com>
2024-09-15 14:33:49 +0900

    :sparkles: Feat: 나의 카테고리를 가져오는 메서드 구현
    <body>
    카테고리 존재 여부, 내 카테고리 여부를 검증하는 메서드 구현
    
    - 관련 : #307

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/category/service/CategoryManageService.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/category/service/CategoryManageService.java
@@ -14,6 +14,22 @@ import lombok.RequiredArgsConstructor;
 public class CategoryManageService {
 
     private final CategoryService categoryService;
+    private final PaletteService paletteService;
+
+    /**
+     * 나의 카테고리를 조회하는 메서드입니다.
+     * !! 카테고리 존재여부, 나의 카테고리 여부를 검증합니다.
+     * @param memberId
+     * @param categoryId
+     * @return
+     */
+    public Category getMyCategory(Long memberId, Long categoryId){
+        Category category = categoryService.readCategory(categoryId)
+                .orElseThrow(() -> new CategoryException(ErrorStatus.NOT_FOUND_CATEGORY_FAILURE));
+        if (!category.getMember().getId().equals(memberId))
+            throw new CategoryException(ErrorStatus.NOT_USERS_CATEGORY);
+        return category;
+    }
 
     public List<Category> getMyCategories(Long memberId){
         return categoryService.readCategoriesByMemberId(memberId);
