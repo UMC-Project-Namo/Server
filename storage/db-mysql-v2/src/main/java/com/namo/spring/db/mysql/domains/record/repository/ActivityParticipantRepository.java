746f41f6442843cff2aadec192aef8edbc468546
hosung-222 <gik11kr@naver.com>
2024-09-28 00:28:18 +0900

    :bug: Fix: JPA Query create시 버그 수정
    <body>
    boolean 필드에 대해 is 접두어를 사용하지 않고 필드 이름 그대로 쿼리 메서드에 사용하여 버그 발생 수정 -> JPQL 우선 해결
    
    - 관련 : #341

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/record/repository/ActivityParticipantRepository.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/record/repository/ActivityParticipantRepository.java
@@ -3,10 +3,18 @@ package com.namo.spring.db.mysql.domains.record.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.namo.spring.db.mysql.domains.record.entity.Activity;
 import com.namo.spring.db.mysql.domains.record.entity.ActivityParticipant;
 
 public interface ActivityParticipantRepository extends JpaRepository<ActivityParticipant, Long> {
-    List<ActivityParticipant> findByActivityAndIncludedInSettlementAndParticipantIdIn(Activity activity, boolean isInSettlement, List<Long> participantIdList);
+
+    @Query("SELECT ap FROM ActivityParticipant ap WHERE ap.activity = :activity AND ap.isIncludedInSettlement = :includedInSettlement AND ap.participant.id IN :participantIdList")
+    List<ActivityParticipant> findByActivityAndIncludedInSettlementAndParticipantIdIn(
+            @Param("activity") Activity activity,
+            @Param("includedInSettlement") boolean includedInSettlement,
+            @Param("participantIdList") List<Long> participantIdList);
+
 }
