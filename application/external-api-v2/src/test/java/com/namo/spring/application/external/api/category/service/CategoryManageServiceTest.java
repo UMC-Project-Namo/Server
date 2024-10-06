61e077de1ffff0f2d753500f55d184e794fab769
hosung-222 <gik11kr@naver.com>
2024-10-06 22:08:03 +0900

    :white_check_mark: Test: 카테고리 조회 성공 테스트
    <body>
    카테고리 조회 성공 테스트
    
    - 관련 : #361

--- application/external-api-v2/src/test/java/com/namo/spring/application/external/api/category/service/CategoryManageServiceTest.java
+++ application/external-api-v2/src/test/java/com/namo/spring/application/external/api/category/service/CategoryManageServiceTest.java
@@ -1,15 +1,21 @@
 package com.namo.spring.application.external.api.category.service;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import com.namo.spring.application.external.api.category.dto.CategoryRequest;
 import com.namo.spring.db.mysql.domains.category.entity.Category;
 import com.namo.spring.db.mysql.domains.category.entity.Palette;
+import com.namo.spring.db.mysql.domains.category.exception.CategoryException;
 import com.namo.spring.db.mysql.domains.category.service.CategoryService;
 import com.namo.spring.db.mysql.domains.category.service.PaletteService;
 import com.namo.spring.db.mysql.domains.category.type.CategoryStatus;
@@ -46,6 +52,19 @@ class CategoryManageServiceTest {
         palette = new Palette(1L, "default", "#FFFFFF", "defaultPalette");
 
         category = new Category(member, palette, "testCategory", CategoryStatus.ACTIVE, CategoryType.COMMON, false, 3);
+    }
+
+    @Test
+    @DisplayName("카테고리 조회 성공")
+    void successGetMyCategory() {
+        Long memberId = 1L;
+        Long categoryId = 1L;
+
+        when(categoryService.readCategory(categoryId)).thenReturn(Optional.of(category));
+
+        Category result = categoryManageService.getMyCategory(memberId, categoryId);
+
+        assertEquals(category, result);
     }
 
 }
