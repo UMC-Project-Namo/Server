bd7f27b4ef13e99b7e87b0044954f85ed7f79c2c
hosung-222 <gik11kr@naver.com>
2024-10-06 22:09:12 +0900

    :test_tube: Test: 카테고리 조회 실패 테스트 (내 카테고리가 아닐때)
    <body>
    내것이 아닌 카테고리에 대해 카테고리 조회 실패 테스트
    
    - 관련 : #361

--- application/external-api-v2/src/test/java/com/namo/spring/application/external/api/category/service/CategoryManageServiceTest.java
+++ application/external-api-v2/src/test/java/com/namo/spring/application/external/api/category/service/CategoryManageServiceTest.java
@@ -78,4 +78,17 @@ class CategoryManageServiceTest {
         assertThrows(CategoryException.class, () -> categoryManageService.getMyCategory(memberId, categoryId));
     }
 
+    @Test
+    @DisplayName("카테고리 조회 실패 : 존재하는 카테고리지만 내 카테고리가 아닐때")
+    void failGetMyCategory_WhenCategoryDoesNotBelongToUser() {
+        Long memberId = 2L;  // 요청한 사용자 ID
+        Long categoryId = 1L;  // 조회할 카테고리 ID
+
+        // 카테고리 조회 시 mock 설정 (해당 카테고리가 반환되도록)
+        when(categoryService.readCategory(categoryId)).thenReturn(Optional.of(category));
+
+        // 테스트 실행: 해당 카테고리가 다른 유저의 카테고리일 경우 예외가 발생하는지 확인
+        assertThrows(CategoryException.class, () -> categoryManageService.getMyCategory(memberId, categoryId));
+    }
+
 }
