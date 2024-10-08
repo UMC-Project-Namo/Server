e27b9f4bbc72a4bc60ce6c0fc6830942a1f55dfa
joowojr <joowojr@gmail.com>
2024-10-08 22:25:36 +0900

    :sparkles: Feat: 게스트 모임 정보 조회 dto 및 converter 구현
    <footer>
    - #364

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/guest/converter/GuestMeetingResponseConverter.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/guest/converter/GuestMeetingResponseConverter.java
@@ -11,43 +11,13 @@ import com.namo.spring.db.mysql.domains.schedule.entity.Schedule;
 
 public class GuestMeetingResponseConverter {
 
-    public static List<GuestMeetingResponse.GetMonthlyMeetingParticipantScheduleDto> toGetMonthlyMeetingParticipantScheduleDtos(
-            List<ScheduleParticipantQuery> participantsWithSchedule, Schedule curSchedule, Long curMemberId) {
-        Map<Long, List<ScheduleParticipantQuery>> scheduleIdAndParticipant
-                = participantsWithSchedule.stream()
-                .collect(Collectors.groupingBy(participant -> participant.getSchedule().getId()));
-        return scheduleIdAndParticipant.entrySet().stream()
-                .map(entry -> toGetMonthlyMeetingParticipantScheduleDto(entry.getValue().get(0).getSchedule(),
-                        entry.getValue(), curSchedule))
-                .collect(Collectors.toList());
-    }
-
-    public static GuestMeetingResponse.GetMonthlyMeetingParticipantScheduleDto toGetMonthlyMeetingParticipantScheduleDto(
-            Schedule schedule, List<ScheduleParticipantQuery> participant, Schedule curSchedule) {
-        return GuestMeetingResponse.GetMonthlyMeetingParticipantScheduleDto.builder()
+    public static GuestMeetingResponse.GetMeetingScheduleInfoDto toGetMeetingScheduleInfoDto(
+            Schedule schedule) {
+        return GuestMeetingResponse.GetMeetingScheduleInfoDto.builder()
                 .scheduleId(schedule.getId())
                 .title(schedule.getTitle())
-                .startDate(DateUtil.toSeconds(schedule.getPeriod().getStartDate()))
-                .endDate(DateUtil.toSeconds(schedule.getPeriod().getEndDate()))
-                .interval(schedule.getPeriod().getDayInterval())
-                .participants(toUserParticipantDtos(participant))
-                .isCurMeetingSchedule(schedule.getId().equals(curSchedule.getId()))
-                .build();
-    }
-
-    private static List<GuestMeetingResponse.ParticipantDto> toUserParticipantDtos(
-            List<ScheduleParticipantQuery> participant) {
-        return participant.stream()
-                .map(GuestMeetingResponseConverter::toUserParticipantDto)
-                .collect(Collectors.toList());
-    }
-
-    private static GuestMeetingResponse.ParticipantDto toUserParticipantDto(ScheduleParticipantQuery
-            participant) {
-        return GuestMeetingResponse.ParticipantDto.builder()
-                .participantId(participant.getParticipantId())
-                .nickname(participant.getNickname())
-                .colorId(participant.getParticipantPaletteId())
+                .startDate(schedule.getPeriod().getStartDate())
+                .imageUrl(schedule.getImageUrl())
                 .build();
     }
 }
--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/guest/dto/GuestMeetingResponse.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/guest/dto/GuestMeetingResponse.java
@@ -1,46 +1,28 @@
 package com.namo.spring.application.external.api.guest.dto;
 
-import java.util.List;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 public class GuestMeetingResponse {
 
     @AllArgsConstructor
     @Getter
     @Builder
-    @Schema(title = "월간 일정 조회 응답 DTO")
-    public static class GetMonthlyMeetingParticipantScheduleDto {
+    @Schema(title = "모임 일정 조회 응답 DTO")
+    public static class GetMeetingScheduleInfoDto {
         @Schema(description = "일정 ID")
         private Long scheduleId;
         @Schema(description = "일정 이름", example = "나모 정기 회의")
         private String title;
-        @Schema(description = "일정 시작일, unix 타임스탬프 형식")
-        private Long startDate;
-        @Schema(description = "일정 종료일, unix 타임스탬프 형식")
-        private Long endDate;
-        @Schema(description = "시작일과 종료일 차이")
-        private Long interval;
-        @Schema(description = "일정 참여자 목록")
-        private List<ParticipantDto> participants;
-        @Schema(description = "현재 조회하는 모임 일정인지의 여부")
-        private Boolean isCurMeetingSchedule = false;
+        @Schema(description = "일정 시작일")
+        private LocalDateTime startDate;
+        @Schema(description = "모임 이미지")
+        private String imageUrl;
     }
 
-    @AllArgsConstructor
-    @Getter
-    @Builder
-    @Schema(title = "모임 일정 조회 - 일정의 참여자 목록")
-    public static class ParticipantDto {
-        @Schema(description = "참여자 ID")
-        private Long participantId;
-        @Schema(description = "닉네임")
-        private String nickname;
-        @Schema(description = "색상 ID")
-        private Long colorId;
-    }
 }
