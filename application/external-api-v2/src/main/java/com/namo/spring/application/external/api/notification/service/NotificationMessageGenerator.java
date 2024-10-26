609efd95c0c760dec456b9f802406ebef282ef51
joowojr <joowojr@gmail.com>
2024-10-26 23:50:15 +0900

    :sparkles: Feature: 기획 바탕으로 알림 메세지 템플릿 구현
    <footer>
    - 관련 : #391

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/notification/service/NotificationMessageGenerator.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/notification/service/NotificationMessageGenerator.java
@@ -14,21 +14,23 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class NotificationMessageGenerator {
-    //TODO : 기획 바탕으로 메세지 템플릿 수정 필요 - 주원
+
     private final Map<NotificationType, String> templates = new HashMap<>();
 
     @PostConstruct
     public void init() {
-        templates.put(NotificationType.FRIEND_REQUEST, "%s님이 친구 요청을 보냈습니다.");
-        templates.put(NotificationType.FRIEND_REQUEST_ACCEPTED, "%s님이 친구 요청을 수락하였습니다.");
-        templates.put(NotificationType.SCHEDULE_REMINDER, "%s %s");
+        templates.put(NotificationType.FRIEND_REQUEST, "%s님으로부터 친구 요청이 도착했어요");
+        templates.put(NotificationType.FRIEND_REQUEST_ACCEPTED, "%s님이 친구 요청을 수락했어요");
+        templates.put(NotificationType.SCHEDULE_REMINDER, "'%s' 일정이 %s 남았어요!");
+        templates.put(NotificationType.GUEST_IS_INVITED, "'%s'에 새로운 게스트가 있습니다");
+        templates.put(NotificationType.SCHEDULE_RECORD_REMINDER, "일정이 종료되었나요? 오늘 일정에 새로운 기록을 남겨보세요");
     }
 
-    public String getScheduleReminderTemplate(String startTime, String title) {
-        return String.format(templates.get(NotificationType.SCHEDULE_REMINDER), startTime, title);
+    public String getScheduleReminderMessageBody(String title, String reminderTime) {
+        return String.format(templates.get(NotificationType.SCHEDULE_REMINDER), title, reminderTime);
     }
 
-    public String getFriendshipTemplate(NotificationType type, String memberName) {
+    public String getFriendshipMessageBody(NotificationType type, String memberName) {
         return String.format(templates.get(type), memberName);
     }
 
