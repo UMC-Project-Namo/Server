f2486c339294f2865d3216c11eb6e2fff031f8e6
joowojr <joowojr@gmail.com>
2024-10-27 11:36:36 +0900

    :sparkles: Feature: 푸시알림 정보 조회 dto 뎁스 추가
    <footer>
    - 관련 : #391

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/converter/NotificationResponseConverter.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/converter/NotificationResponseConverter.java
@@ -9,11 +9,19 @@ import java.util.List;
 public class NotificationResponseConverter {
 
     public static NotificationResponse.GetNotificationSettingInfoDto toGetNotificationSettingInfoDto(Member member, Device device) {
+
         return NotificationResponse.GetNotificationSettingInfoDto.builder()
-                .deviceId(device.getId())
                 .notificationEnabled(member.isNotificationEnabled())
-                .deviceType(device.getReceiverDeviceType().name())
-                .deviceToken(device.getReceiverDeviceToken())
+                .deviceInfo(toDeviceInfo(device))
                 .build();
+    }
+
+    public static NotificationResponse.DeviceInfo toDeviceInfo(Device device) {
+        return device==null ? null :
+                NotificationResponse.DeviceInfo.builder()
+                        .deviceId(device.getId())
+                        .deviceToken(device.getReceiverDeviceToken())
+                        .deviceType(device.getReceiverDeviceType().name())
+                        .build();
     }
 }
--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/dto/NotificationResponse.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/dto/NotificationResponse.java
@@ -14,13 +14,22 @@ public class NotificationResponse {
     public static class GetNotificationSettingInfoDto {
         @Schema(description = "푸시 알림 활성화 여부")
         private boolean notificationEnabled;
+        @Schema(description = "기기 정보")
+        private DeviceInfo deviceInfo;
+    }
+
+
+    @Getter
+    @Builder
+    public static class DeviceInfo {
         @Schema(description = "기기 ID")
         private Long deviceId;
         @Schema(description = "기기 type, WEB / IOS / ANDROID")
         private String deviceType;
         @Schema(description = "기기 토큰")
         private String deviceToken;
+        @Schema(description = "기기 상세 정보")
+        private String deviceAgent;
     }
-
 
 }
