08fa6c3db65d8fc9cb4fec3ae2f40768b8c18157
hosung-222 <gik11kr@naver.com>
2024-10-29 18:20:05 +0900

    :memo: Docs: 친구 관계가 아닐때 에러뜨는 내용 스웨거 명세
    <body>
    친구 관계가 아닌 상대방을 조회할 때 에러에 대해 명세합니다.
    
    - 관련 : #397

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/controller/FriendController.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/controller/FriendController.java
@@ -92,9 +92,10 @@ public class FriendController {
         return ResponseDto.onSuccess("친구 요청을 거절했습니다.");
     }
 
-    @Operation(summary = "내 친구 목록을 조회합니다. [20명씩 조회]", description = "내 친구 리스트를 보는 API 입니다. "
-            + "즐겨찾기에 등록된 친구가 가장 먼저 나오고, 그 후에 닉네임 사전 순으로 정렬됩니다. "
-            + "검색 조건이 있는경우 search에 넣어 주세요 아닌경우 사용하지 않으셔도 됩니다.")
+    @Operation(summary = "내 친구 목록을 조회합니다. [20명씩 조회]", description = """
+            내 친구 리스트를 보는 API 입니다.\s
+            즐겨찾기에 등록된 친구가 가장 먼저 나오고, 그 후에 닉네임 사전 순으로 정렬됩니다.\s
+            검색 조건이 있는경우 search에 넣어 주세요 아닌경우 사용하지 않으셔도 됩니다.""")
     @GetMapping("")
     public ResponseDto<FriendshipResponse.FriendListDto> getFriendList(
             @AuthenticationPrincipal SecurityUserDetails member,
@@ -133,7 +134,10 @@ public class FriendController {
         return ResponseDto.onSuccess("친구 삭제 완료");
     }
 
-    @Operation(summary = "친구의 스케줄 카테고리 정보를 조회", description = "친구가 등록한 카테고리를 조회합니다(공개된 카테고리만 조회됩니다)")
+    @Operation(summary = "친구의 스케줄 카테고리 정보를 조회", description = "친구가 등록한 카테고리를 조회합니다(공개된 카테고리만 조회됩니다) \n 친구가 아닌경우 조회할 수 없습니다.")
+    @ApiErrorCodes(value = {
+            NOT_FOUND_FRIENDSHIP_FAILURE,
+    })
     @GetMapping("/{friendId}/categories")
     public ResponseDto<List<FriendCategoryResponse.CategoryInfoDto>> getFriendCategories(
             @AuthenticationPrincipal SecurityUserDetails member,
