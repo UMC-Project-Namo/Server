5d6d67b5ec1569f1bb30034d2d26e6b1cbabc2b8
hosung-222 <gik11kr@naver.com>
2024-11-01 20:44:54 +0900

    :sparkles: Feat: S3 멀티파트 업로드 완료 요청 API 구현
    <body>
    S3에 멀티파트 업로드를 완료하는 요청을 보내는 API 구현 완료
    
    - 관련 : #404

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/image/controller/S3Controller.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/image/controller/S3Controller.java
@@ -7,12 +7,14 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.namo.spring.application.external.global.annotation.swagger.ApiErrorCodes;
 import com.namo.spring.core.common.response.ResponseDto;
+import com.namo.spring.core.infra.common.aws.dto.MultipartCompleteRequest;
 import com.namo.spring.core.infra.common.aws.dto.MultipartStartResponse;
 import com.namo.spring.core.infra.common.aws.s3.S3Uploader;
 
@@ -38,21 +40,48 @@ public class S3Controller {
             INTERNET_SERVER_ERROR})
     @GetMapping("/generate-presigned-url")
     public ResponseDto<String> generatePresignedUrl(
-            @Parameter(description = "이미지 종류입니다 {activity: 활동 이미지, diary: 일기 이미지, cover: 커버 이미지, profile: 프로필 이미지} 입력 가능합니다.", example = "activity")
+            @Parameter(description = "업로드할 이미지의 종류를 선택해 주세요. 예: 'activity' (활동 이미지), 'diary' (일기 이미지), 'cover' (커버 이미지), 'profile' (프로필 이미지)",
+                    example = "activity")
             @RequestParam String prefix,
+            @Parameter(description = "업로드할 파일의 이름을 입력해 주세요.", example = "example.jpg")
             @RequestParam String fileName) {
         String preSignedUrl = s3Service.getPreSignedUrl(prefix, fileName);
         return ResponseDto.onSuccess(preSignedUrl);
     }
 
     @PostMapping("/pre-signed/start")
+    @Operation(
+            summary = "S3 Presigned URLs 생성 요청 (Part 업로드용)",
+            description = "이 API는 클라이언트가 큰 파일을 여러 파트로 나누어 S3에 업로드할 수 있도록 각 파트에 대한 Presigned URL을 생성합니다. \n"
+                    + "파일을 원하는 만큼의 파트로 나누기 위해 partCount에 파트 수를 입력해 주세요. 반환된 각 URL을 사용해 각 파트를 업로드한 후, "
+                    + "업로드가 완료되면 반드시 '/pre-signed/complete' API를 호출하여 최종 업로드를 완료해야 합니다. 그렇지 않으면 S3에 임시로 저장된 파일이 일정 기간(7일) 후 삭제될 수 있습니다."
+    )
     public ResponseDto<MultipartStartResponse> start(
+            @Parameter(description = "업로드할 파일의 이름을 입력해 주세요.", example = "example.jpg")
             @RequestParam String fileName,
-            @Parameter(description = "이미지 종류입니다 {activity: 활동 이미지, diary: 일기 이미지, cover: 커버 이미지, profile: 프로필 이미지} 입력 가능합니다.", example = "activity")
+            @Parameter(description = "업로드할 이미지의 종류를 선택해 주세요. 예: 'activity' (활동 이미지), 'diary' (일기 이미지), 'cover' (커버 이미지), 'profile' (프로필 이미지)",
+                    example = "activity")
             @RequestParam String prefix,
+            @Parameter(description = "파일을 몇 개의 파트로 나누어 업로드할지 입력해 주세요.", example = "3")
             @RequestParam int partCount
     ) {
         return ResponseDto.onSuccess(s3Service.getPreSignedUrls(prefix, fileName, partCount));
     }
 
+    @PostMapping("/pre-signed/complete")
+    @Operation(
+            summary = "S3 멀티파트 업로드 완료 요청",
+            description = "이 API는 S3에 멀티파트 업로드를 완료하는 요청을 보냅니다. 클라이언트는 모든 파트를 Presigned URLs을 통해 업로드한 후 이 API를 호출하여 업로드를 최종 완료해야 합니다. \n"
+                    + "업로드가 완료되지 않으면 임시 저장된 파일이 일정 기간(7일) 후 삭제될 수 있습니다."
+    )
+    public ResponseDto<String> complete(
+            @Parameter(description = "업로드 완료를 위한 요청 정보를 JSON 형식으로 전달합니다. 이 정보에는 파일 이름, 업로드 ID, 각 파트의 ETag 정보가 포함되어야 합니다.")
+            @RequestBody MultipartCompleteRequest request
+    ) {
+        return ResponseDto.onSuccess(s3Service.completeMultipartUpload(
+                request.getFileName(),
+                request.getUploadId(),
+                request.getPartETags()
+        ));
+    }
 }
