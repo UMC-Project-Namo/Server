086e330d63ac18cab962450546f4debd7506b8f8
hosung-222 <gik11kr@naver.com>
2024-11-28 15:41:56 +0900

    :sparkles: Feat: 유저 값 Update API 스펙 명세
    스펙 명세
    
    - 관련 : #417

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/controller/ProfileController.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/user/controller/ProfileController.java
@@ -2,9 +2,12 @@ package com.namo.spring.application.external.api.user.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.namo.spring.application.external.api.user.dto.ProfileRequest;
 import com.namo.spring.application.external.api.user.dto.ProfileResponse;
 import com.namo.spring.application.external.api.user.usecase.ProfileUseCase;
 import com.namo.spring.application.external.global.common.security.authentication.SecurityUserDetails;
@@ -29,5 +32,15 @@ public class ProfileController {
             @AuthenticationPrincipal SecurityUserDetails member
     ){
         return ResponseDto.onSuccess(profileUseCase.getProfile(member.getUserId()));
+    }
+
+    @PatchMapping("")
+    @Operation(summary = "내 프로필 수정", description = "내 프로필 정보를 수정합니다.")
+    public ResponseDto<String> updateProfile(
+            @AuthenticationPrincipal SecurityUserDetails member,
+            @RequestBody ProfileRequest.UpdateProfileDto request
+    ){
+        profileUseCase.updateProfile(member.getUserId(), request);
+        return ResponseDto.onSuccess("내 정보 수정 완료");
     }
 }
