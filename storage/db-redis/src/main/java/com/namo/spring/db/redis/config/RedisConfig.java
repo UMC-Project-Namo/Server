838d8d7d33cbfd16bba77e03d95efc8cd6932b5e
hosung-222 <gik11kr@naver.com>
2024-12-01 23:16:53 +0900

    :sparkles: Feat: CustomObjectMapper 생성
    class type 정보와 JavaTimeModule을 포함한 CustomObjectMapper 빈 등록
    
    - 관련 : #421

--- storage/db-redis/src/main/java/com/namo/spring/db/redis/config/RedisConfig.java
+++ storage/db-redis/src/main/java/com/namo/spring/db/redis/config/RedisConfig.java
@@ -19,6 +19,12 @@ import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator;
+import com.fasterxml.jackson.databind.jsontype.PolymorphicTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.namo.spring.db.redis.common.annotation.DomainRedisCacheManager;
 import com.namo.spring.db.redis.common.annotation.DomainRedisConnectionFactory;
 import com.namo.spring.db.redis.common.annotation.DomainRedisTemplate;
@@ -55,9 +61,8 @@ public class RedisConfig {
 		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
 
 		redisTemplate.setConnectionFactory(redisConnectionFactory());
-
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
-		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(customObjectMapper()));
 		return redisTemplate;
 	}
 
@@ -67,11 +72,29 @@ public class RedisConfig {
 		RedisCacheConfiguration cacheConfiguration =
 			RedisCacheConfiguration.defaultCacheConfig()
 				.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()))
+				.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(customObjectMapper())))
 				.entryTtl(Duration.ofHours(3L));
 
 		return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(cf)
 			.cacheDefaults(cacheConfiguration)
 			.build();
 	}
+
+
+    @Bean
+    public ObjectMapper customObjectMapper(){
+        PolymorphicTypeValidator ptv = BasicPolymorphicTypeValidator
+                .builder()
+                .allowIfSubType(Object.class)
+                .build();
+
+        return new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) // 혹시라도 의도치 않거나 알 수 없는 정보가 들어와 시리얼라이즈를 할 수 없게 될 경우를 대비한 설정값
+                .registerModule(new JavaTimeModule())
+                .activateDefaultTyping(ptv, ObjectMapper.DefaultTyping.NON_FINAL)
+                .disable(SerializationFeature.WRITE_DATE_TIMESTAMPS_AS_NANOSECONDS)
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .disable(SerializationFeature.WRITE_DATE_KEYS_AS_TIMESTAMPS); // redis를 활용할 객체들에 날짜 정보가 TimeStamp 형식으로 적용되어있을 경우 그대로 RedisTemplate을 사용하면 에러가 발생하므로 그것에 대비하기 위한 설정값
+    }
+    
 }
