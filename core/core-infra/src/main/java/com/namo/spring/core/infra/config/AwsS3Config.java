9a103baa9224f54d8fa68f48bc93304e3beab509
hosung-222 <gik11kr@naver.com>
2024-12-05 19:30:08 +0900

    :sparkles: Feat: s3 도메인 주소 변환 기능 구현
    환경변수의 값을 가지고 s3 도메인 주소를 반환하는 메서드 구현
    
    - 관련 : #423

--- core/core-infra/src/main/java/com/namo/spring/core/infra/config/AwsS3Config.java
+++ core/core-infra/src/main/java/com/namo/spring/core/infra/config/AwsS3Config.java
@@ -1,5 +1,7 @@
 package com.namo.spring.core.infra.config;
 
+import java.security.PublicKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,17 +21,21 @@ public class AwsS3Config {
     private final String region;
     @Getter
     private final String bucketName;
+    @Getter
+    private final String cdnPath;
 
     public AwsS3Config(
             @Value("${cloud.aws.credentials.access-key}") String accessKey,
             @Value("${cloud.aws.credentials.secret-key}") String secretKey,
             @Value("${cloud.aws.region.static}") String region,
-            @Value("${cloud.aws.s3.bucket}") String bucketName
+            @Value("${cloud.aws.s3.bucket}") String bucketName,
+            @Value("${cloud.aws.cloudfront}") String cdnName
     ) {
         this.accessKey = accessKey;
         this.secretKey = secretKey;
         this.region = region;
         this.bucketName = bucketName;
+        this.cdnPath = cdnName;
     }
 
     @Bean
@@ -43,6 +49,10 @@ public class AwsS3Config {
                 .withRegion(region)
                 .withCredentials(new AWSStaticCredentialsProvider(awsS3Credentials()))
                 .build();
+    }
+
+    public String getS3Domain(){
+        return String.format("%s.s3.%s.amazonaws.com", bucketName, region);
     }
 }
 
