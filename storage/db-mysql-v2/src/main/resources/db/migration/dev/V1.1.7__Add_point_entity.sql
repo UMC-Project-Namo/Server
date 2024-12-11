7a49fa70c6eda00894a7348b559bfab18009bdb9
hosung-222 <gik11kr@naver.com>
2024-12-11 21:24:08 +0900

    :sparkles: Feat: point, point_transaction 생성 flyway 구현 v1.1.7
    V1.1.7__Add_point_entity.sql
    
    - 관련 : #425

---
+++ storage/db-mysql-v2/src/main/resources/db/migration/dev/V1.1.7__Add_point_entity.sql
@@ -0,0 +1,19 @@
+-- Point 테이블 생성
+CREATE TABLE point (
+                       id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                       member_id BIGINT NOT NULL,
+                       balance BIGINT NOT NULL DEFAULT 0,
+                       CONSTRAINT fk_point_member FOREIGN KEY (member_id) REFERENCES member (id) ON DELETE CASCADE
+);
+
+-- PointTransaction 테이블 생성
+CREATE TABLE point_transaction (
+                                   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                                   point_id BIGINT NOT NULL,
+                                   transaction_type VARCHAR(20) NOT NULL,
+                                   point_status VARCHAR(20) NOT NULL,
+                                   amount BIGINT NOT NULL,
+                                   transaction_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                                   description VARCHAR(255),
+                                   CONSTRAINT fk_point_transaction_point FOREIGN KEY (point_id) REFERENCES point (id) ON DELETE CASCADE
+);
