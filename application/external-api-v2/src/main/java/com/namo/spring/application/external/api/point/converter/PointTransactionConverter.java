50dfb920bcb6f0a2b7c14e8316b1ab3d814f111b
hosung-222 <gik11kr@naver.com>
2024-12-18 22:46:24 +0900

    :sparkles: Feat: 포인트 충전 요청 내역 조회 DTO Converter 구현
    builder 패턴 컨버터 구현
    
    - 관련 : #425

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/point/converter/PointTransactionConverter.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/point/converter/PointTransactionConverter.java
@@ -1,9 +1,15 @@
 package com.namo.spring.application.external.api.point.converter;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.namo.spring.application.external.api.point.dto.PointResponse;
 import com.namo.spring.db.mysql.domains.point.entity.Point;
 import com.namo.spring.db.mysql.domains.point.entity.PointTransaction;
 import com.namo.spring.db.mysql.domains.point.enums.TransactionStatus;
 import com.namo.spring.db.mysql.domains.point.enums.TransactionType;
+import com.namo.spring.db.mysql.domains.user.entity.Member;
 
 public class PointTransactionConverter {
 
@@ -14,6 +20,35 @@ public class PointTransactionConverter {
                 .transactionStatus(TransactionStatus.PENDING)
                 .amount(amount)
                 .description(TransactionType.CHARGE.getDescription())
+                .build();
+    }
+
+    public static PointResponse.ChargePointRequestListDto toChargePointRequestListDto(
+            Page<PointTransaction> pointTransactions) {
+        List<PointResponse.ChargePointRequestDto> chargePointRequests = pointTransactions
+                .getContent()
+                .stream()
+                .map(PointTransactionConverter::toChargePointRequestDto)
+                .toList();
+
+        return PointResponse.ChargePointRequestListDto.builder()
+                .chargePointRequests(chargePointRequests)
+                .currentPage(pointTransactions.getNumber() + 1)
+                .pageSize(pointTransactions.getSize())
+                .totalItems(pointTransactions.getTotalElements())
+                .totalPages(pointTransactions.getTotalPages())
+                .build();
+    }
+
+    public static PointResponse.ChargePointRequestDto toChargePointRequestDto(PointTransaction pointTransaction){
+        Member pointMember = pointTransaction.getPoint().getMember();
+        return PointResponse.ChargePointRequestDto.builder()
+                .profileImage(pointMember.getProfileImage())
+                .nickname(pointMember.getNickname())
+                .tag(pointMember.getTag())
+                .pointTransactionId(pointTransaction.getId())
+                .amount(pointTransaction.getAmount())
+                .requestDate(pointTransaction.getTransactionDate())
                 .build();
     }
 }
--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/point/dto/PointResponse.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/point/dto/PointResponse.java
@@ -1,6 +1,9 @@
 package com.namo.spring.application.external.api.point.dto;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -13,7 +16,24 @@ public class PointResponse {
     @Getter
     @AllArgsConstructor
     @NoArgsConstructor
+    public static class ChargePointRequestListDto{
+        private List<ChargePointRequestDto> chargePointRequests;
+        @Schema(description = "총 페이지 수", example = "5")
+        private int totalPages;
+        @Schema(description = "현재 페이지 번호", example = "1")
+        private int currentPage;
+        @Schema(description = "한 페이지당 항목 수", example = "20")
+        private int pageSize;
+        @Schema(description = "전체 항목 수", example = "100")
+        private long totalItems;
+    }
+
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class ChargePointRequestDto {
+        private String profileImage;
         private String nickname;
         private String tag;
         private Long pointTransactionId;
