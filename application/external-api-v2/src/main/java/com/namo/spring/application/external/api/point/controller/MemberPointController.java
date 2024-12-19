d659368e90708387cf62e8bc5f116e8e6f06f1c3
hosung-222 <gik11kr@naver.com>
2024-12-19 22:26:23 +0900

    :memo: Docs: 포인트 관련 스웨거 문서화
    포인트 API 스웨거 문서화
    
    - 관련 : #425

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/point/controller/MemberPointController.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/point/controller/MemberPointController.java
@@ -1,5 +1,7 @@
 package com.namo.spring.application.external.api.point.controller;
 
+import static com.namo.spring.core.common.code.status.ErrorStatus.*;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,13 +9,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.namo.spring.application.external.api.point.usecase.PointChargeUseCase;
+import com.namo.spring.application.external.global.annotation.swagger.ApiErrorCodes;
 import com.namo.spring.core.common.response.ResponseDto;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 import lombok.RequiredArgsConstructor;
 
-@Tag(name = "11. 포인트 ", description = "포인트 관련 API")
+@Tag(name = "11. 포인트 - 유저", description = "포인트 관련 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v2/users/points")
@@ -21,9 +26,14 @@ public class MemberPointController {
 
     private final PointChargeUseCase pointChargeUseCase;
 
+    @Operation(summary = "포인트 충전 요청", description = "포인트 충전 요청을 진행합니다.")
+    @ApiErrorCodes(value = {
+            NOT_FOUND_ACTIVE_USER_FAILURE,
+    })
     @PostMapping("/charge/{amount}")
     public ResponseDto<String> requestChargePoints(
             @AuthenticationPrincipal Long memberId,
+            @Parameter(description = "충전 요청 포인트 금액", example = "5000")
             @PathVariable Long amount
     ){
         pointChargeUseCase.requestChargePoints(memberId, amount);
