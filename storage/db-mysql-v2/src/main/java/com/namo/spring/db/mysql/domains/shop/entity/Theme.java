14ab965a80fb60fbfddd6471388a0a04f0a05f00
hosung-222 <gik11kr@naver.com>
2025-01-01 20:50:34 +0900

    :sparkles: Feat: 테마 엔티티 구현 Theme.class
    테마 저장용 엔티티 구현
    
    - 관련 : #427

---
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/entity/Theme.java
@@ -0,0 +1,56 @@
+package com.namo.spring.db.mysql.domains.shop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import com.namo.spring.db.mysql.domains.shop.enums.ThemeType;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+public class Theme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; // 테마 이름
+
+    private String description; // 테마 설명
+
+    @Column(nullable = false)
+    private Integer price; // 테마 가격
+
+    private String previewImageUrl; // 미리보기 이미지 URL
+
+    @Column(nullable = false)
+    private String detailImageUrl; // 상세 이미지 URL
+
+    @Enumerated(EnumType.STRING)
+    private ThemeType type; // 테마 유형 (배경 테마, 프로필 테마 등)
+
+    @Builder
+    public Theme(String name, String description, Integer price, String previewImageUrl, String detailImageUrl, ThemeType type) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.previewImageUrl = previewImageUrl;
+        this.detailImageUrl = detailImageUrl;
+        this.type = type;
+    }
+
+
+}
