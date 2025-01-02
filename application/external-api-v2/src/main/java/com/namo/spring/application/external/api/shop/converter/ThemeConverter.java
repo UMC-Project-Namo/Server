b5cedc87f22127b4ef3d8e9aaf6f134b9d844deb
hosung-222 <gik11kr@naver.com>
2025-01-02 23:04:52 +0900

    :sparkles: Feat: 조회된 테마를 dto로 변환하는 Converter 구현
    builder 패턴 사용한 converter 구현
    
    - 관련 : #427

---
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/shop/converter/ThemeConverter.java
@@ -0,0 +1,36 @@
+package com.namo.spring.application.external.api.shop.converter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+
+import com.namo.spring.application.external.api.shop.dto.ThemeResponse;
+import com.namo.spring.db.mysql.domains.shop.entity.Theme;
+
+public class ThemeConverter {
+
+    public static ThemeResponse.ThemeDtoList toThemeDtoList(Page<Theme> themes) {
+        List<ThemeResponse.ThemeResponseDto> themeDtoList = themes.stream()
+                .map(ThemeConverter::toThemeResponseDto)
+                .collect(Collectors.toList());
+
+        return ThemeResponse.ThemeDtoList.builder()
+                .themes(themeDtoList)
+                .totalPages(themes.getTotalPages())
+                .currentPage(themes.getNumber() + 1)
+                .pageSize(themes.getSize())
+                .totalItems(themes.getTotalElements())
+                .build();
+    }
+
+    public static ThemeResponse.ThemeResponseDto toThemeResponseDto(Theme theme) {
+        return ThemeResponse.ThemeResponseDto.builder()
+            .id(theme.getId())
+            .name(theme.getName())
+            .description(theme.getDescription())
+            .price(theme.getPrice())
+            .previewImageUrl(theme.getPreviewImageUrl())
+            .build();
+    }
+}
