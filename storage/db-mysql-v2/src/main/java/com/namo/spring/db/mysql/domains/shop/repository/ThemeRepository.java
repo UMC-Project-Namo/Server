a4c23b836928f0bf5c57b32b48aa7d5faf1ab1dd
hosung-222 <gik11kr@naver.com>
2025-01-02 23:01:15 +0900

    :sparkles: Feat: 테마 판매 상태와 종류 별 조회 도메인 기능 구현
    테마의 status, type으로 페이징하여 조회하는 도메인 로직 구현
    
    - 관련 : #427

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/repository/ThemeRepository.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/repository/ThemeRepository.java
@@ -1,8 +1,14 @@
 package com.namo.spring.db.mysql.domains.shop.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.namo.spring.db.mysql.domains.shop.entity.Theme;
+import com.namo.spring.db.mysql.domains.shop.enums.ThemeStatus;
+import com.namo.spring.db.mysql.domains.shop.enums.ThemeType;
 
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
+
+    Page<Theme> findByTypeAndStatus(ThemeType themeType, Pageable pageable, ThemeStatus themeStatus);
 }
--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/service/ThemeService.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/service/ThemeService.java
@@ -1,6 +1,12 @@
 package com.namo.spring.db.mysql.domains.shop.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.namo.spring.core.common.annotation.DomainService;
+import com.namo.spring.db.mysql.domains.shop.entity.Theme;
+import com.namo.spring.db.mysql.domains.shop.enums.ThemeStatus;
+import com.namo.spring.db.mysql.domains.shop.enums.ThemeType;
 import com.namo.spring.db.mysql.domains.shop.repository.ThemeRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -11,4 +17,7 @@ public class ThemeService {
 
     private final ThemeRepository themeRepository;
 
+    public Page<Theme> findByTypeAndStatus(ThemeType themeType, Pageable pageable, ThemeStatus themeStatus) {
+        return themeRepository.findByTypeAndStatus(themeType, pageable, themeStatus);
+    }
 }
