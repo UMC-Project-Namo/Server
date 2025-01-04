fa86a1c6f6323b209a8cd4cd2e8e18c0bde27e1a
hosung-222 <gik11kr@naver.com>
2025-01-04 22:26:55 +0900

    :sparkles: Feat: 테마 정보와 해당 테마의 소유 여부를 반환하는 비즈니스 로직 구현
    테마 정보와 소유 여부 반환
    
    - 관련 : #427

--- application/external-api-v2/src/main/java/com/namo/spring/application/external/api/shop/service/ThemeManageService.java
+++ application/external-api-v2/src/main/java/com/namo/spring/application/external/api/shop/service/ThemeManageService.java
@@ -6,9 +6,12 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.namo.spring.core.common.code.status.ErrorStatus;
 import com.namo.spring.db.mysql.domains.shop.entity.Theme;
 import com.namo.spring.db.mysql.domains.shop.enums.ThemeStatus;
 import com.namo.spring.db.mysql.domains.shop.enums.ThemeType;
+import com.namo.spring.db.mysql.domains.shop.exceptions.ThemeException;
+import com.namo.spring.db.mysql.domains.shop.service.MemberThemeService;
 import com.namo.spring.db.mysql.domains.shop.service.ThemeService;
 
 import lombok.RequiredArgsConstructor;
@@ -18,9 +21,18 @@ import lombok.RequiredArgsConstructor;
 public class ThemeManageService {
 
     private final ThemeService themeService;
+    private final MemberThemeService memberThemeService;
 
     public Page<Theme> getSellingThemesByType(ThemeType themeType, int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
-        return themeService.findByTypeAndStatus(themeType, pageable, ThemeStatus.ACTIVE);
+        return themeService.findByTypeAndStatus(themeType, pageable, ThemeStatus.SELLING);
+    }
+
+    public Theme getThemeByIdWithOwnership(Long memberId, Long themeId) {
+        Theme theme = themeService.findByIdAndStatus(themeId, ThemeStatus.SELLING)
+                .orElseThrow(() -> new ThemeException(ErrorStatus.NOT_FOUND_THEME));
+        boolean isOwned = memberThemeService.purchaseInfo(memberId, themeId);
+        theme.setOwned(isOwned);
+        return theme;
     }
 }
