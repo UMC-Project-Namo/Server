cbf9f54f7d13fd42d912fd2b5d3a9afaf0665eef
hosung-222 <gik11kr@naver.com>
2025-01-04 22:25:06 +0900

    :sparkles: Feat: 테마 상태, id로 조회하는 도메인 로직 구현
    findByIdAndStatus()
    
    - 관련 : #427

--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/repository/ThemeRepository.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/repository/ThemeRepository.java
@@ -1,5 +1,7 @@
 package com.namo.spring.db.mysql.domains.shop.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +13,6 @@ import com.namo.spring.db.mysql.domains.shop.enums.ThemeType;
 public interface ThemeRepository extends JpaRepository<Theme, Long> {
 
     Page<Theme> findByTypeAndStatus(ThemeType themeType, Pageable pageable, ThemeStatus themeStatus);
+
+    Optional<Theme> findByIdAndStatus(Long id, ThemeStatus themeStatus);
 }
--- storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/service/ThemeService.java
+++ storage/db-mysql-v2/src/main/java/com/namo/spring/db/mysql/domains/shop/service/ThemeService.java
@@ -1,5 +1,7 @@
 package com.namo.spring.db.mysql.domains.shop.service;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -16,6 +18,10 @@ import lombok.RequiredArgsConstructor;
 public class ThemeService {
 
     private final ThemeRepository themeRepository;
+
+    public Optional<Theme> findByIdAndStatus(Long id, ThemeStatus themeStatus) {
+        return themeRepository.findByIdAndStatus(id, ThemeStatus.SELLING);
+    }
 
     public Page<Theme> findByTypeAndStatus(ThemeType themeType, Pageable pageable, ThemeStatus themeStatus) {
         return themeRepository.findByTypeAndStatus(themeType, pageable, themeStatus);
